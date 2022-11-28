967aa8cd43fd18c7d41222ba9ddcba7cebcbc538
devxsby <devxsby@gmail.com>
2022-11-28 15:46:17 +0900

    [Chore] #8- 접근 제한자 public 으로 변경

--- SOPT-Stamp-iOS/Projects/Modules/DSKit/Sources/Components/CustomButton.swift
+++ SOPT-Stamp-iOS/Projects/Modules/DSKit/Sources/Components/CustomButton.swift
@@ -10,11 +10,11 @@ import UIKit
 
 import Core
 
-final class CustomButton: UIButton {
+public class CustomButton: UIButton {
     
     // MARK: - Initialize
 
-    init(title: String) {
+    public init(title: String) {
         super.init(frame: .zero)
         self.setUI(title)
     }
@@ -29,14 +29,14 @@ final class CustomButton: UIButton {
 extension CustomButton {
     /// 버튼의 enable 여부 설정
     @discardableResult
-    func setEnabled(_ isEnabled: Bool) -> Self {
+    public func setEnabled(_ isEnabled: Bool) -> Self {
         self.isEnabled = isEnabled
         return self
     }
     
     /// 버튼의 Title 변경
     @discardableResult
-    func changeTitle(attributedString: NSAttributedString) -> Self {
+    public func changeTitle(attributedString: NSAttributedString) -> Self {
         self.setAttributedTitle(attributedString, for: .normal)
         return self
     }
