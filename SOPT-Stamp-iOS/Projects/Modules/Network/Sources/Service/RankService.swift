bbe467b64813ec89923ab38d57ff4fcb6ab566e2
L-j-h-c <ckrgkswnsgh@naver.com>
2022-12-03 21:56:16 +0900

    [Setting] #24 - 네트워크 모듈 라우팅 클래스 정의

---
+++ SOPT-Stamp-iOS/Projects/Modules/Network/Sources/Service/RankService.swift
@@ -0,0 +1,24 @@
+//
+//  RankService.swift
+//  Network
+//
+//  Created by Junho Lee on 2022/12/03.
+//  Copyright © 2022 SOPT-Stamp-iOS. All rights reserved.
+//
+
+import Foundation
+import Combine
+
+import Alamofire
+import Moya
+
+public typealias DefaultRankService = BaseService<RankAPI>
+
+public protocol RankService {
+    
+}
+
+extension DefaultRankService: RankService {
+    
+}
+
