159696243442374508b5f4f73321140c3f190c46
Subin Yang <cindy010670@gmail.com>
2022-12-05 19:56:00 +0900

    [Chore] #31 - 불필요한 StringLiterals 삭제, 추가

--- SOPT-Stamp-iOS/Projects/Core/Sources/Literals/StringLiterals.swift
+++ SOPT-Stamp-iOS/Projects/Core/Sources/Literals/StringLiterals.swift
@@ -9,25 +9,15 @@
 import Foundation
 
 public struct I18N {
-    public struct Sample {
+    public struct Default {
         public static let error = "에러"
         public static let networkError = "네트워크 오류가 발생했습니다."
+        public static let delete = "삭제"
+        public static let cancel = "취소"
     }
     
     public struct TextFieldView {
         public static let verify = "확인"
-    }
-    
-    public struct Navi {
-        public static let post = "공지"
-    }
-    
-    public struct Search {
-        public static let placeholder = "검색어 입력"
-        public static let cancel = "취소"
-        public static let countUnit = " 건"
-        public static let enterSearch = "검색어를 입력해 주세요."
-        public static let noSearchData = "등록된 게시물이 없습니다"
     }
     
     public struct SignUp {
@@ -52,5 +42,6 @@ public struct I18N {
         public static let mission = "미션"
         public static let missionComplete = "미션 완료"
         public static let editComplte = "수정 완료"
+        public static let deleteTitle = "달성한 미션을 삭제하시겠습니까?"
     }
 }
