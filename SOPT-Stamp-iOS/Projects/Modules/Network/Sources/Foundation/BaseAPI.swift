9870d71a124bbc755f18b377ef4b7d64d9cd1203
L-j-h-c <ckrgkswnsgh@naver.com>
2022-12-20 16:11:41 +0900

    [Feat] #28 - MissionListAPI 구현

--- SOPT-Stamp-iOS/Projects/Modules/Network/Sources/Foundation/BaseAPI.swift
+++ SOPT-Stamp-iOS/Projects/Modules/Network/Sources/Foundation/BaseAPI.swift
@@ -11,9 +11,11 @@ import Moya
 import Foundation
 
 public enum APIType {
-    case notice
     case auth
-    case alert
+    case mission
+    case rank
+    case stamp
+    case user
 }
 
 public protocol BaseAPI: TargetType {
@@ -25,21 +27,44 @@ extension BaseAPI {
         var base = Config.Network.baseURL
         
         switch Self.apiType {
-        case .alert:
-            base += "alert"
-        case .notice:
-            base += "notice"
         case .auth:
-            base += "auth"
+            base += "/auth"
+        case .mission:
+            base += "/mission"
+        case .rank:
+            base += "/rank"
+        case .stamp:
+            base += "/stamp"
+        case .user:
+            base += "/user"
         }
         
         guard let url = URL(string: base) else {
             fatalError("baseURL could not be configured")
         }
+        
         return url
     }
     
     public var headers: [String: String]? {
         return ["Content-Type": "application/json"]
+    }
+}
+
+public enum HeaderType {
+    case json
+    case jsonUserId(userId: Int)
+    case userId(userId: Int)
+    
+    public var value: [String: String] {
+        switch self {
+        case .json:
+            return ["Content-Type": "application/json"]
+        case .jsonUserId(let userId):
+            return ["Content-Type": "application/json",
+                    "userId": String(userId)]
+        case .userId(let userId):
+            return ["userId": String(userId)]
+        }
     }
 }
--- SOPT-Stamp-iOS/Projects/Modules/Network/Sources/Service/MissionService.swift
+++ SOPT-Stamp-iOS/Projects/Modules/Network/Sources/Service/MissionService.swift
@@ -15,9 +15,21 @@ import Moya
 public typealias DefaultMissionService = BaseService<MissionAPI>
 
 public protocol MissionService {
-    
+    func fetchAllMissionList(userId: Int) -> AnyPublisher<MissionListEntity, Error>
+    func fetchCompleteMissionList(userId: Int) -> AnyPublisher<MissionListEntity, Error>
+    func fetchIncompleteMissionList(userId: Int) -> AnyPublisher<MissionListEntity, Error>
 }
 
 extension DefaultMissionService: MissionService {
+    public func fetchAllMissionList(userId: Int) -> AnyPublisher<MissionListEntity, Error> {
+        requestObjectInCombine(MissionAPI.fetchMissionList(type: .all, userId: userId))
+    }
     
+    public func fetchCompleteMissionList(userId: Int) -> AnyPublisher<MissionListEntity, Error> {
+        requestObjectInCombine(MissionAPI.fetchMissionList(type: .complete, userId: userId))
+    }
+    
+    public func fetchIncompleteMissionList(userId: Int) -> AnyPublisher<MissionListEntity, Error> {
+        requestObjectInCombine(MissionAPI.fetchMissionList(type: .incomplete, userId: userId))
+    }
 }
