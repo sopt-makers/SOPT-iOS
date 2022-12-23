b5080a528f47ea662fc68b2b5b603751b237deb0
devxsby <devxsby@gmail.com>
2022-12-23 20:33:55 +0900

    [Feat] #46- 시작 루트 뷰컨트롤러 SignIn 뷰로 변경

--- SOPT-Stamp-iOS/Projects/SOPT-Stamp-iOS/Sources/Application/SceneDelegate.swift
+++ SOPT-Stamp-iOS/Projects/SOPT-Stamp-iOS/Sources/Application/SceneDelegate.swift
@@ -20,7 +20,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         
         window = UIWindow(frame: scene.coordinateSpace.bounds)
         window?.windowScene = scene
-        let rootVC = ModuleFactory.shared.makeMissionListVC(sceneType: .default)
+        let rootVC = ModuleFactory.shared.makeSignInVC()
         window?.rootViewController = UINavigationController(rootViewController: rootVC)
         window?.makeKeyAndVisible()
     }
