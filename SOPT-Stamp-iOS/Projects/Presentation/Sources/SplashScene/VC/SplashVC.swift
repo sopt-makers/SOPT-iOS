826460a06ee68b33ee52692c664d69345af4494a
Sejin Lee <lsj8706@naver.com>
2023-01-18 16:37:39 +0900

    [Feat] #114 - SplashVC 화면 전환 함수 분리

--- SOPT-Stamp-iOS/Projects/Presentation/Sources/SplashScene/VC/SplashVC.swift
+++ SOPT-Stamp-iOS/Projects/Presentation/Sources/SplashScene/VC/SplashVC.swift
@@ -35,7 +35,6 @@ public class SplashVC: UIViewController {
         self.setUI()
         self.setLayout()
         self.setNavigationBar()
-        self.setDelay()
     }
 }
 
@@ -65,42 +64,23 @@ extension SplashVC {
         self.navigationController?.navigationBar.isHidden = true
     }
     
-    private func setDelay() {
+    private func presentNoticePopUp() {
         let popUp = factory.makeNoticePopUpVC()
         popUp.modalPresentationStyle = .overFullScreen
-        popUp.setData(type: .recommendUpdate, content: """
- 안녕하세요! makers입니다.
-
-이번 업데이트에서는 아래와 같은 내용들이
-반영되었습니다.
-
-공지 기능 추가
-솝트 내 공지를 한 곳에서 확인하세요.
-출석 기능 추가
-솝트 출석을 더 편하게 관리하세요.
-
-
-많은 관심 부탁드리며
-피드백은 언제나 환영입니다!
-
-이번 업데이트에서는 아래와 같은 내용들이
-반영되었습니다.
-이번 업데이트에서는 아래와 같은 내용들이
-반영되었습니다.
-이번 업데이트에서는 아래와 같은 내용들이
-반영되었습니다.
-""")
+        popUp.setData(type: .recommendUpdate, content: "공지 내용")
         self.present(popUp, animated: true)
-        
-//        DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
-//            let needAuth = UserDefaultKeyList.Auth.userId == nil
-//            if !needAuth {
-//                let navigation = UINavigationController(rootViewController: self.factory.makeMissionListVC(sceneType: .default))
-//                ViewControllerUtils.setRootViewController(window: self.view.window!, viewController: navigation, withAnimation: true)
-//            } else {
-//                let nextVC = self.factory.makeOnboardingVC()
-//                self.navigationController?.pushViewController(nextVC, animated: true)
-//            }
-//        }
+    }
+    
+    private func setDelay() {
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+            let needAuth = UserDefaultKeyList.Auth.userId == nil
+            if !needAuth {
+                let navigation = UINavigationController(rootViewController: self.factory.makeMissionListVC(sceneType: .default))
+                ViewControllerUtils.setRootViewController(window: self.view.window!, viewController: navigation, withAnimation: true)
+            } else {
+                let nextVC = self.factory.makeOnboardingVC()
+                self.navigationController?.pushViewController(nextVC, animated: true)
+            }
+        }
     }
 }
