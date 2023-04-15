405db2bb083e111a51007ce97f1a4e918f46830c
Sejin Lee <lsj8706@naver.com>
2023-04-15 16:18:10 +0900

    [Chore] #151 - userInfoLabel에 attributedText 적용하는 함수 분리

--- SOPT-iOS/Projects/Features/MainFeature/Sources/MainScene/Cells/UsersHistory/UserHistoryHeaderView.swift
+++ SOPT-iOS/Projects/Features/MainFeature/Sources/MainScene/Cells/UsersHistory/UserHistoryHeaderView.swift
@@ -57,44 +57,35 @@ extension UserHistoryHeaderView {
         if let name = name, let months = months {
             let text = (userType == .visitor) ? I18N.Main.encourage : I18N.Main.userHistory(name: name, months: months)
 
-            let attributedText = NSMutableAttributedString(string: text,
-                                                          attributes: [
-                                                             .foregroundColor: DSKitAsset.Colors.white100.color,
-                                                             .font: UIFont.Main.display2
-                                                          ])
-            attributedText.addAttribute(.font,
-                                         value: UIFont.Main.display1,
-                                         range: (text as NSString).range(of:"\(name)"))
-            
-            attributedText.addAttribute(.font,
-                                         value: UIFont.Main.display1,
-                                        range: (text as NSString).range(of:I18N.Main.hello))
-
-            self.userInfoLabel.attributedText = attributedText
+            setAttributedTextToUserInfoLabel(text: text, name: name)
         } else {
             if userType == .visitor {
                 let text = I18N.Main.encourage
-                let attributedText = NSMutableAttributedString(string: text,
-                                                              attributes: [
-                                                                 .foregroundColor: DSKitAsset.Colors.white100.color,
-                                                                 .font: UIFont.Main.display2
-                                                              ])
-                attributedText.addAttribute(.font,
-                                             value: UIFont.Main.display1,
-                                            range: (text as NSString).range(of: I18N.Main.hello))
-                self.userInfoLabel.attributedText = attributedText
+                setAttributedTextToUserInfoLabel(text: text, name: nil)
             } else if userType == .inactive {
                 let text = I18N.Main.welcome
-                let attributedText = NSMutableAttributedString(string: text,
-                                                              attributes: [
-                                                                 .foregroundColor: DSKitAsset.Colors.white100.color,
-                                                                 .font: UIFont.Main.display2
-                                                              ])
-                attributedText.addAttribute(.font,
-                                             value: UIFont.Main.display1,
-                                            range: (text as NSString).range(of: I18N.Main.hello))
-                self.userInfoLabel.attributedText = attributedText
+                setAttributedTextToUserInfoLabel(text: text, name: nil)
             }
         }
+    }
+    
+    func setAttributedTextToUserInfoLabel(text: String, name: String?) {
+        let attributedText = NSMutableAttributedString(string: text,
+                                                      attributes: [
+                                                         .foregroundColor: DSKitAsset.Colors.white100.color,
+                                                         .font: UIFont.Main.display2
+                                                      ])
+        
+        if let name = name {
+            attributedText.addAttribute(.font,
+                                         value: UIFont.Main.display1,
+                                         range: (text as NSString).range(of:"\(name)"))
+        }
+        
+        attributedText.addAttribute(.font,
+                                     value: UIFont.Main.display1,
+                                    range: (text as NSString).range(of:I18N.Main.hello))
+
+        self.userInfoLabel.attributedText = attributedText
     }
 }
