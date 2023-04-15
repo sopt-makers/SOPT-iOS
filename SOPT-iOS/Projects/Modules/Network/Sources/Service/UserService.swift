53ac3d133eda034cf54066121a7ce225097de660
Sejin Lee <lsj8706@naver.com>
2023-04-15 17:04:24 +0900

    [Feat] #147 - UserService에 메인뷰 조회 함수 추가

--- SOPT-iOS/Projects/Modules/Network/Sources/Service/UserService.swift
+++ SOPT-iOS/Projects/Modules/Network/Sources/Service/UserService.swift
@@ -21,6 +21,7 @@ public protocol UserService {
     func editSentence(sentence: String) -> AnyPublisher<EditSentenceEntity, Error>
     func getNicknameAvailable(nickname: String) -> AnyPublisher<Int, Error>
     func changeNickname(nickname: String) -> AnyPublisher<Int, Error>
+    func getUserMainInfo() -> AnyPublisher<MainEntity, Error>
 }
 
 extension DefaultUserService: UserService {
@@ -38,5 +39,9 @@ extension DefaultUserService: UserService {
     
     public func changeNickname(nickname: String) -> AnyPublisher<Int, Error> {
         requestObjectInCombineNoResult(.changeNickname(nickname: nickname))
+    }
+    
+    public func getUserMainInfo() -> AnyPublisher<MainEntity, Error> {
+        requestObjectInCombine(.getUserMainInfo)
     }
 }
