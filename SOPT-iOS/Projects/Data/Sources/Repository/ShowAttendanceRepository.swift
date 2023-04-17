a03b0c542b66e2012c1b38360c5d9bb28d32c8d4
devxsby <devxsby@gmail.com>
2023-04-17 22:00:16 +0900

    [Chore] #171- mock data 변경

--- SOPT-iOS/Projects/Data/Sources/Repository/ShowAttendanceRepository.swift
+++ SOPT-iOS/Projects/Data/Sources/Repository/ShowAttendanceRepository.swift
@@ -26,21 +26,14 @@ public class ShowAttendanceRepository {
 extension ShowAttendanceRepository: ShowAttendanceRepositoryInterface {
     public func fetchAttendanceScheduleModel() -> AnyPublisher<AttendanceScheduleModel, Error> {
         return Future<AttendanceScheduleModel, Error> { promise in
-//            promise(.success(AttendanceScheduleModel(type: "HAS_ATTENDANCE",
-//                                                     location: "단국대학교 혜당관",
-//                                                     name: "3차 세미나",
-//                                                     startDate: "2023-04-06T14:14:00", endDate: "2023-04-06T18:00:00", message: "",
-//                                                     attendances: [TodayAttendanceModel(status: "ATTENDANCE", attendedAt: "2023-04-13T14:12:09"),
-//                                                                   TodayAttendanceModel(status: "ABSENT", attendedAt:
-//            "2023-04-13T14:10:04")])))
-            
+
             promise(.success(AttendanceScheduleModel(type: "HAS_ATTENDANCE",
-                                                     location: "한국대학교 혜당관",
-                                                     name: "2차 행사",
-                                                     startDate: "2023-04-06T14:14:00", endDate: "2023-04-06T18:00:00",
-                                                     message: "행사도 참여하고, 출석점수도 받고, 일석이조!",
-                                                     attendances: [TodayAttendanceModel(status: "ATTENDANCE", attendedAt: "2023-04-13T14:12:09"),
-                                                                   TodayAttendanceModel(status: "ABSENT", attendedAt: "2023-04-13T14:10:04")])))
+                                                     location: "솝트대학교 IT 창업관",
+                                                     name: "3차 세미나",
+                                                     startDate: "2023-04-06T14:00:00", endDate: "2023-04-06T18:00:00",
+                                                     message: "",
+                                                     attendances: [TodayAttendanceModel(status: "ATTENDANCE", attendedAt: "2023-04-13T14:00:00"),
+                                                                   TodayAttendanceModel(status: "ABSENT", attendedAt: "2023-04-13T14:02:00")])))
 
 //            promise(.success(AttendanceScheduleModel(type: "NO_SESSION",
 //                                                     location: "",
@@ -57,15 +50,16 @@ extension ShowAttendanceRepository: ShowAttendanceRepositoryInterface {
     
     public func fetchAttendanceScoreModel() -> AnyPublisher<Domain.AttendanceScoreModel, Error> {
         return Future<AttendanceScoreModel, Error> { promise in
-            promise(.success(AttendanceScoreModel.init(part: "iOS",
-                                                       generation: 31,
-                                                       name: "윤수빈",
-                                                       score: 2.0,
-                                                       total: TotalScoreModel(attendance: 2, absent: 0, tardy: 1, participate: 1),
-                                                       attendances: [AttendanceModel(attribute: "ETC", name: "솝커톤", status: "TARDY", date: "4월 22일"),
-                                                                     AttendanceModel(attribute: "SEMINAR", name: "iOS 2차 세미나", status: "ATTENDANCE", date: "4월 15일"),
-                                                                     AttendanceModel(attribute: "SEMINAR", name: "iOS 1차 세미나", status: "ATTENDANCE", date: "4월 8일"),
-                                                                     AttendanceModel(attribute: "ETC", name: "OT", status: "PARTICIPATE", date: "4월 1일")])))
+            promise(.success(AttendanceScoreModel.init(part: "디자인",
+                                                       generation: 32,
+                                                       name: "김솝트",
+                                                       score: 1.0,
+                                                       total: TotalScoreModel(attendance: 2, absent: 1, tardy: 1, participate: 1),
+                                                       attendances: [AttendanceModel(attribute: "ETC", name: "솝커톤", status: "PARTICIPATE", date: "4월 22일"),
+                                                                     AttendanceModel(attribute: "ETC", name: "1차 행사", status: "ATTENDANCE", date: "4월 15일"),
+                                                                     AttendanceModel(attribute: "SEMINAR", name: "2차 세미나", status: "ATTENDANCE", date: "4월 08일"),
+                                                                     AttendanceModel(attribute: "SEMINAR", name: "1차 세미나", status: "ABSENT", date: "4월 01일"),
+                                                                     AttendanceModel(attribute: "ETC", name: "OT", status: "TARDY", date: "3월 25일")])))
         }.eraseToAnyPublisher()
 //        return self.attendanceService.fetchAttendanceScore()
 //            .map { $0.toDomain() }
