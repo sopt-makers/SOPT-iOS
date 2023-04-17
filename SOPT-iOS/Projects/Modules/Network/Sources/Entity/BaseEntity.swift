01395071a0f271f1578f4f0d5336dffafb6b992a
김영인 <0inn1220@gmail.com>
2023-04-17 14:59:36 +0900

    [Feat] #159 - 출석하기 Data 및 Domain 추가

---
+++ SOPT-iOS/Projects/Modules/Network/Sources/Entity/BaseEntity.swift
@@ -0,0 +1,26 @@
+//
+//  BaseEntity.swift
+//  Network
+//
+//  Created by 김영인 on 2023/04/16.
+//  Copyright © 2023 SOPT-iOS. All rights reserved.
+//
+
+import Foundation
+
+public struct BaseEntity<T: Decodable>: Decodable {
+    public let success: Bool
+    public let message: String
+    public let data: T
+    
+    enum CodingKeys: String, CodingKey {
+        case success, message, data
+    }
+    
+    public init(from decoder: Decoder) throws {
+        let values = try decoder.container(keyedBy: CodingKeys.self)
+        success = try values.decode(Bool.self, forKey: .success)
+        message = try values.decode(String.self, forKey: .message)
+        data = try values.decodeIfPresent(T.self, forKey: .data) ?? "non-data" as! T
+    }
+}
