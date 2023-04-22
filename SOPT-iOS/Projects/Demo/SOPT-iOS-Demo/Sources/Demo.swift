530c9a69f3cdd59113edfb5059dc81bcac1bb709
Sejin Lee <lsj8706@naver.com>
2023-04-23 00:16:17 +0900

    [Feat] #189 - 데모 프로젝트를 앱 테스트 환경으로 설정

--- SOPT-iOS/Projects/Demo/SOPT-iOS-Demo/Sources/Demo.swift
+++
@@ -1,9 +0,0 @@
-//
-//  Demo.swift
-//  SOPT-iOS-Demo
-//
-//  Created by 김영인 on 2023/04/14.
-//  Copyright © 2023 SOPT-iOS. All rights reserved.
-//
-
-import Foundation
--- SOPT-iOS/Tuist/ProjectDescriptionHelpers/Project+Templates.swift
+++ SOPT-iOS/Tuist/ProjectDescriptionHelpers/Project+Templates.swift
@@ -29,7 +29,7 @@ public extension Project {
         // MARK: - App
         
         if targets.contains(.app) {
-            let bundleSuffix = name.contains("Demo") ? "demo" : "release"
+            let bundleSuffix = name.contains("Demo") ? "test" : "release"
             let infoPlist = name.contains("Demo") ? Project.demoInfoPlist : Project.appInfoPlist
             let settings = baseSettings.setProvisioning()
             
