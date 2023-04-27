a5c7da89e75923b9f60d035288782830a0d51f1f
L-j-h-c <ckrgkswnsgh@naver.com>
2023-04-28 00:42:47 +0900

    [Fix] #219 - 토큰 재발급 수정사항 반영, 헤더 adpating

--- SOPT-iOS/Projects/Modules/Network/Sources/Foundation/AlamoInterceptor.swift
+++ SOPT-iOS/Projects/Modules/Network/Sources/Foundation/AlamoInterceptor.swift
@@ -16,7 +16,26 @@ public class AlamoInterceptor: RequestInterceptor {
     
     public typealias AdapterResult = Swift.Result<URLRequest, Error>
     private var authService = DefaultAuthService()
-
+    
+    public func adapt(_ urlRequest: URLRequest, for session: Alamofire.Session, completion: @escaping (Result<URLRequest, Error>) -> Void) {
+        var adaptedRequest = urlRequest
+        adaptedRequest = validateHeader(&adaptedRequest)
+        completion(.success(adaptedRequest))
+    }
+    
+    // Note: 토큰 재발급 시 AccessToken 갱신
+    // @준호
+    private func validateHeader(_ urlRequest: inout URLRequest) -> URLRequest {
+        let headers = urlRequest.headers.map {
+            guard $0.name == "Authorization" else {
+                return $0
+            }
+            return HTTPHeader(name: $0.name, value: UserDefaultKeyList.Auth.appAccessToken ?? "")
+        }
+        urlRequest.headers = HTTPHeaders(headers)
+        return urlRequest
+    }
+    
     public func retry(_ request: Alamofire.Request, for session: Alamofire.Session, dueTo error: Swift.Error, completion: @escaping (RetryResult) -> Void) {
         // token 재발급 API가 아니며 && 로그인 실패가 아니며 && 토큰이 만료된 경우(401)
         guard let pathComponents = request.request?.url?.pathComponents,
