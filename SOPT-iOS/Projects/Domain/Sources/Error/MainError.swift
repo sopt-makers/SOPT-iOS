cd39974fe1423a3c1d2a093a65389caeeb4d2797
Sejin Lee <lsj8706@naver.com>
2023-05-19 15:18:03 +0900

    [Feat] #236 - MainError에 DebugDescription 추가

--- SOPT-iOS/Projects/Domain/Sources/Error/MainError.swift
+++ SOPT-iOS/Projects/Domain/Sources/Error/MainError.swift
@@ -13,3 +13,20 @@ public enum MainError: Error {
     case unregisteredUser // 플그 미동록 유저
     case authFailed // 토큰 재발급 실패 등 인증 에러
 }
+
+extension MainError: CustomNSError {
+    public var errorUserInfo: [String : Any] {
+        func getDebugDescription() -> String {
+            switch self {
+            case .networkError(let message):
+                return  message ?? ""
+            case .unregisteredUser:
+                return "플그 미등록"
+            case .authFailed:
+                return "인증 실패"
+            }
+        }
+
+        return [NSDebugDescriptionErrorKey: getDebugDescription()]
+    }
+}
