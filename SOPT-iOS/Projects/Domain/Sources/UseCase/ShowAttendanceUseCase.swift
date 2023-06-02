8c8adae32e2adc63807bc8accca9053e5fbe11b7
김영인 <0inn1220@gmail.com>
2023-06-02 11:12:09 -0400

    [Feat] #243 - ProgressBar 라인 컬러 구현

--- SOPT-iOS/Projects/Domain/Sources/UseCase/ShowAttendanceUseCase.swift
+++ SOPT-iOS/Projects/Domain/Sources/UseCase/ShowAttendanceUseCase.swift
@@ -15,7 +15,7 @@ enum TodayAttendanceType: String {
     case absent = "ABSENT"
 }
 
-enum TakenAttendanceType: Int, CaseIterable {
+public enum TakenAttendanceType: Int, CaseIterable {
     case notYet
     case first
     case second
@@ -27,6 +27,7 @@ public protocol ShowAttendanceUseCase {
     func fetchLectureRound(lectureId: Int)
     var attendanceScheduleFetched: PassthroughSubject<AttendanceScheduleModel, Error> { get set }
     var todayAttendances: PassthroughSubject<[AttendanceStepModel], Never> { get set }
+    var takenAttendanceType: PassthroughSubject<TakenAttendanceType, Never> { get set }
     var attendanceScoreFetched: PassthroughSubject<AttendanceScoreModel, Error> { get set }
     var lectureRound: PassthroughSubject<AttendanceRoundModel?, Never> { get set }
     var lectureRoundErrorTitle: PassthroughSubject<String, Never> { get set }
@@ -51,6 +52,7 @@ public class DefaultShowAttendanceUseCase {
     
     public var attendanceScheduleFetched = PassthroughSubject<AttendanceScheduleModel, Error>()
     public var todayAttendances = PassthroughSubject<[AttendanceStepModel], Never>()
+    public var takenAttendanceType = PassthroughSubject<TakenAttendanceType, Never>()
     public var attendanceScoreFetched = PassthroughSubject<AttendanceScoreModel, Error>()
     public var lectureRound = PassthroughSubject<AttendanceRoundModel?, Never>()
     public var lectureRoundErrorTitle = PassthroughSubject<String, Never>()
@@ -73,13 +75,16 @@ extension DefaultShowAttendanceUseCase: ShowAttendanceUseCase {
                 print("completion: fetchAttendanceSchedule \(event)")
             }, receiveValue: { owner, model in
                 owner.attendanceScheduleFetched.send(model)
+                
+                /// 몇차 출석까지 했는지 확인
+                owner.setTakenAttendance(model.attendances)
+                
                 /// 출석 점수 반영되는 날만 출석 프로그레스바 정보 필요
                 if model.type == SessionType.hasAttendance.rawValue {
                     owner.setAttendances(model.attendances)
                 }
                 /// 출석하는 날(세미나, 행사, 솝커톤, 데모데이)에 출석버튼 보이게
                 if model.type != SessionType.noSession.rawValue {
-                    owner.setTakenAttendance(model.attendances)
                     owner.fetchLectureRound(lectureId: model.id)
                 }
             })
@@ -130,6 +135,7 @@ extension DefaultShowAttendanceUseCase: ShowAttendanceUseCase {
         /// 출석 전
         if attendanceData.isEmpty {
             self.todayAttendances.send(Default.before)
+            self.takenAttendanceType.send(takenAttendance)
             return
         }
         /// 출석 후 해당 정보 담기 (ex. ✓, 🅧)
@@ -165,6 +171,7 @@ extension DefaultShowAttendanceUseCase: ShowAttendanceUseCase {
         }
         
         self.todayAttendances.send(attendances)
+        self.takenAttendanceType.send(takenAttendance)
     }
     
     /*
