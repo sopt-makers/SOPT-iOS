f3e9d1d5f815be2b35f7b80ab7fc1f5031e29134
Sejin Lee <lsj8706@naver.com>
2023-06-03 22:21:25 +0900

    [Feat] #242 - Prod 앱 푸시 알림 세팅

--- SOPT-iOS/Projects/SOPT-iOS/Sources/Application/AppDelegate.swift
+++ SOPT-iOS/Projects/SOPT-iOS/Sources/Application/AppDelegate.swift
@@ -9,6 +9,7 @@ import UIKit
 
 import Sentry
 import FirebaseCore
+import FirebaseMessaging
 
 import Network
 import Core
@@ -18,7 +19,22 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     
     func application( _ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         configureSentry()
+        
+        // FCM
         FirebaseApp.configure()
+        Messaging.messaging().delegate = self
+        
+        UNUserNotificationCenter.current().delegate = self
+        
+        let authOptions: UNAuthorizationOptions = [.alert, .badge, .sound]
+        UNUserNotificationCenter.current().requestAuthorization(options: authOptions) { granted, error in
+            if let error = error {
+                print(error)
+            }
+            
+            granted ? print("FCM-알림 등록 완료") : print("FCM-알림 등록 실패")
+        }
+        application.registerForRemoteNotifications()
         return true
     }
     
@@ -54,5 +70,28 @@ extension AppDelegate {
             options.failedRequestStatusCodes = [ httpStatusCodeRange ]
             options.enableAutoBreadcrumbTracking = true
         }
+    }
+}
+
+// MARK: - Firebase
+
+extension AppDelegate: UNUserNotificationCenterDelegate, MessagingDelegate {
+    func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
+        Messaging.messaging().apnsToken = deviceToken
+    }
+    
+    func messaging(_ messaging: Messaging, didReceiveRegistrationToken fcmToken: String?) {
+        print("FCM-Token 서버로 전달 필요: \(fcmToken ?? "토큰을 받지 못함.."))")
+    }
+    
+    func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification) async -> UNNotificationPresentationOptions {
+        let userInfo = notification.request.content.userInfo
+        print("FCM-푸시 알림 페이로드: \(userInfo)")
+        return([.badge, .banner, .list, .sound])
+    }
+    
+    func userNotificationCenter(_ center: UNUserNotificationCenter, didReceive response: UNNotificationResponse) async {
+        let userInfo = response.notification.request.content.userInfo
+        print("FCM-푸시 알림 페이로드: \(userInfo)")
     }
 }
