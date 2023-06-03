40a938e685578553c7f15cbc114ac2177b6d4ea3
Sejin Lee <lsj8706@naver.com>
2023-06-03 22:08:08 +0900

    [Feat] #242 - FCM을 위해 entitlements 세팅과 Other Link Flags 수정

--- SOPT-iOS/Tuist/ProjectDescriptionHelpers/Project+Templates.swift
+++ SOPT-iOS/Tuist/ProjectDescriptionHelpers/Project+Templates.swift
@@ -42,6 +42,7 @@ public extension Project {
                 infoPlist: .extendingDefault(with: infoPlist),
                 sources: ["Sources/**/*.swift"],
                 resources: [.glob(pattern: "Resources/**", excluding: [])],
+                entitlements: "\(name).entitlements",
                 scripts: [.Sentry],
                 dependencies: [
                     internalDependencies,
