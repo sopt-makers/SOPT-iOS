ef30fb743759fe58b0c6befd99e7790e7dbe8eaf
Sejin Lee <lsj8706@naver.com>
2023-06-27 22:49:55 +0900

    [Feat] #257 - UserService에 푸시토큰 등록 함수 추가

--- SOPT-iOS/Projects/Modules/Network/Sources/Service/UserService.swift
+++ SOPT-iOS/Projects/Modules/Network/Sources/Service/UserService.swift
@@ -17,12 +17,12 @@ public typealias DefaultUserService = BaseService<UserAPI>
 
 public protocol UserService {
     func fetchSoptampUser() -> AnyPublisher<SoptampUserEntity, Error>
-    
     func editSentence(sentence: String) -> AnyPublisher<EditSentenceEntity, Error>
     func getNicknameAvailable(nickname: String) -> AnyPublisher<Int, Error>
     func changeNickname(nickname: String) -> AnyPublisher<Int, Error>
     func getUserMainInfo() -> AnyPublisher<MainEntity, Error>
     func withdraw() -> AnyPublisher<Int, Error>
+    func registerPushToken(with token: String) -> AnyPublisher<Int, Error>
 }
 
 extension DefaultUserService: UserService {
@@ -48,5 +48,9 @@ extension DefaultUserService: UserService {
   
     public func withdraw() -> AnyPublisher<Int, Error> {
         requestObjectInCombineNoResult(.withdrawal)
+    }
+ 
+    public func registerPushToken(with token: String) -> AnyPublisher<Int, Error> {
+        requestObjectInCombineNoResult(.registerPushToken(token: token))
     }
 }
