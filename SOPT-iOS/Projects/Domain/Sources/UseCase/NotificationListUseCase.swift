99c12bec3dcf7fa49771db4be4619e24ca46b72e
Lee SeJin <lsj8706@naver.com>
2023-09-09 22:34:48 +0900

    [Feat] #257 - NotificationListUseCase 구현

--- SOPT-iOS/Projects/Domain/Sources/UseCase/NotificationListUseCase.swift
+++ SOPT-iOS/Projects/Domain/Sources/UseCase/NotificationListUseCase.swift
@@ -11,13 +11,17 @@ import Combine
 import Core
 
 public protocol NotificationListUseCase {
-
+    var notificationList: PassthroughSubject<[NotificationListModel], Never> { get set }
+    
+    func getNotificationList(page: Int)
 }
 
 public class DefaultNotificationListUseCase {
   
     private let repository: NotificationListRepositoryInterface
     private var cancelBag = CancelBag()
+    
+    public var notificationList = PassthroughSubject<[NotificationListModel], Never>()
   
     public init(repository: NotificationListRepositoryInterface) {
         self.repository = repository
@@ -25,5 +29,13 @@ public class DefaultNotificationListUseCase {
 }
 
 extension DefaultNotificationListUseCase: NotificationListUseCase {
-  
+    // TODO: 에러 핸들링 필요
+    public func getNotificationList(page: Int) {
+        repository.getNotificationList(page: page)
+            .sink { event in
+                print("GetNotificationList State: \(event)")
+            } receiveValue: { [weak self] notificationListModel in
+                self?.notificationList.send(notificationListModel)
+            }.store(in: self.cancelBag)
+    }
 }
