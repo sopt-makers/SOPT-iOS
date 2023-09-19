52e1f2d4be625b6342458fd0c2590f5c458c3ba6
Sejin Lee <lsj8706@naver.com>
2023-09-19 14:11:43 +0900

    [Del] #270 - 마이페이지 플그 미등록 관련 섹션 제거

--- SOPT-iOS/Projects/Features/AppMyPageFeature/Sources/AppMypageScene/AppMyPageViewController.swift
+++ SOPT-iOS/Projects/Features/AppMyPageFeature/Sources/AppMypageScene/AppMyPageViewController.swift
@@ -148,17 +148,6 @@ public final class AppMyPageVC: UIViewController, MyPageViewControllable {
         frame: self.view.frame
     )
     
-    // MARK: For UnregisteredInActive
-    
-    private lazy var etcForUnregisteredInActiveSectionGroup = MypageSectionGroupView(
-        headerTitle: I18N.MyPage.etcSectionGroupTitle,
-        subviews: [
-            self.logoutListItem,
-            self.withDrawalListItem
-        ],
-        frame: self.view.frame
-    )
-    
     public init(
         userType: UserType,
         viewModel: AppMyPageViewModel
