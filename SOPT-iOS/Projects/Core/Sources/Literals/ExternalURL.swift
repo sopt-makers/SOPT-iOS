3cb19b763ae3c1a6592626004d29bc7ea06a3678
Sejin Lee <lsj8706@naver.com>
2023-12-03 20:52:23 +0900

    [Fix] #320 - Dev 앱의 Playground 관련 링크들 분기처리

--- SOPT-iOS/Projects/Core/Sources/Literals/ExternalURL.swift
+++ SOPT-iOS/Projects/Core/Sources/Literals/ExternalURL.swift
@@ -21,6 +21,15 @@ public struct ExternalURL {
     
     public struct SOPT {
         public static let project = "https://sopt.org/project"
+        public static let officialHomepage = "https://sopt.org"
+        public static let review = "https://sopt.org/review"
+        public static let faq = "https://sopt.org/FAQ"
+    }
+    
+    public struct SNS {
+        public static let youtube = "https://m.youtube.com/@SOPTMEDIA"
+        public static let instagram = "https://www.instagram.com/sopt_official"
+        
     }
     
     public struct Playground {
@@ -33,5 +42,10 @@ public struct ExternalURL {
         public static func login(state: String = "") -> String {
             return "\(main)/auth/oauth?redirect_uri=sopt-makers://org.sopt.makers.iOS/oauth2redirect&state=\(state)"
         }
+        
+        public static let project = "\(main)/projects"
+        public static let member = "\(main)/members"
+        public static let group = "\(main)/group?utm_source=playground_group&utm_medium=app_button&utm_campaign=app"
+        public static let playgroundCommunity = main
     }
 }
