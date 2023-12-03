d3da78f71ad5edbcc7dc87869c01ad53fb0c4d7b
Sejin Lee <lsj8706@naver.com>
2023-12-03 16:41:58 +0900

    [Feat] #310 - PokeRelation 열거형 생성

---
+++ SOPT-iOS/Projects/Features/PokeFeature/Sources/Enum/PokeRelation.swift
@@ -0,0 +1,15 @@
+//
+//  PokeRelation.swift
+//  PokeFeature
+//
+//  Created by sejin on 12/3/23.
+//  Copyright © 2023 SOPT-iOS. All rights reserved.
+//
+
+import Foundation
+
+public enum PokeRelation {
+    case newFriend
+    case bestFriend
+    case soulmate
+}
