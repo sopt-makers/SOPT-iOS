aecdd29e2a1f3b2164842a01f4faee19369f61e5
Lee SeJin <lsj8706@naver.com>
2023-12-14 20:49:39 +0900

    [Feat] #325 - PokeFriendsSectionHeaderView 구현

---
+++ SOPT-iOS/Projects/Features/PokeFeature/Sources/PokeMyFriendsScene/Views/PokeFriendsSectionGroupView.swift
@@ -0,0 +1,63 @@
+//
+//  PokeFriendsSectionGroupView.swift
+//  PokeFeature
+//
+//  Created by sejin on 12/14/23.
+//  Copyright © 2023 SOPT-iOS. All rights reserved.
+//
+
+import UIKit
+import Combine
+
+import DSKit
+import Core
+
+public final class PokeFriendsSectionGroupView: UIView {
+    
+    // MARK: - Properties
+    
+    public lazy var rightButtonTap: Driver<PokeRelation> = headerView.rightButtonTap.map { self.relation }.asDriver()
+    
+    private let relation: PokeRelation
+    
+    // MARK: - UI Components
+    
+    private let headerView = PokeFriendsSectionHeaderView()
+    
+    // MARK: - initialization
+    
+    init(pokeRelation: PokeRelation, maxContentsCount: Int) {
+        self.relation = pokeRelation
+        super.init(frame: .zero)
+        self.setUI()
+        self.makeContents(count: maxContentsCount)
+        self.setLayout()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+extension PokeFriendsSectionGroupView {
+    private func setUI() {
+        self.backgroundColor = .clear
+    }
+    
+    private func makeContents(count: Int) {
+        // 셀 넣기
+    }
+    
+    private func setLayout() {
+        self.addSubviews(headerView)
+        
+        headerView.snp.makeConstraints { make in
+            make.top.leading.trailing.equalToSuperview()
+            make.height.equalTo(48)
+        }
+    }
+}
+
+extension PokeFriendsSectionGroupView {
+
+}
