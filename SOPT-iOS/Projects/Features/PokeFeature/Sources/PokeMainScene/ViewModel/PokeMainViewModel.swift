ac76c6ed5f9bc2e0b8d266a3fe1ac02c5d6c6a9b
Lee SeJin <lsj8706@naver.com>
2023-12-20 15:31:40 +0900

    [Feat] #237 - 친구 수에 따른 ChipView 텍스트 분기처리

--- SOPT-iOS/Projects/Features/PokeFeature/Sources/PokeMainScene/ViewModel/PokeMainViewModel.swift
+++ SOPT-iOS/Projects/Features/PokeFeature/Sources/PokeMainScene/ViewModel/PokeMainViewModel.swift
@@ -123,7 +123,14 @@ extension PokeMainViewModel {
     
     private func makeChipInfo(with model: PokeUserModel) -> PokeChipView.ChipType {
         if model.isFirstMeet { // 친구가 아닌 경우
-            return .acquaintance(friendname: model.mutual.first ?? "", relationCount: "\(model.mutual.count)명")
+            switch model.mutual.count {
+            case 0:
+                return .newUser
+            case 1:
+                return .singleFriend(friendName: model.mutual.first ?? "")
+            default:
+                return .acquaintance(friendname: model.mutual.first ?? "", relationCount: "\(model.mutual.count-1)명")
+            }
         }
         
         return .withPokeCount(relation: model.relationName, pokeCount: String(model.pokeNum))
