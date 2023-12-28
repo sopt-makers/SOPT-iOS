4ba2779ff3b2ea3c194d7004a00650d321839691
Lee SeJin <lsj8706@naver.com>
2023-12-28 21:12:28 +0900

    [Fix] #344 - 리프레시 가이드 문구 수정

--- SOPT-iOS/Projects/Core/Sources/Literals/StringLiterals.swift
+++ SOPT-iOS/Projects/Core/Sources/Literals/StringLiterals.swift
@@ -327,7 +327,7 @@ public struct I18N {
         public static let pokeMyFriends = "내 친구를 찔러보세요"
         public static let pokeNearbyFriends = "내 친구의 친구를 찔러보세요"
         public static let emptyFriendDescription = "아직 없어요 T.T\n내 친구가 더 많은 친구가 생길 때까지 기다려주세요"
-        public static let refreshGuide = "화면을 밑으로 당기면\n다른 친구를 볼 수 있어요"
+        public static let refreshGuide = "화면을 당기면\n다른 친구를 볼 수 있어요"
         public static func makingFriendCompleted(name: String) -> String {
             return "찌르기 답장으로\n\(name)님과 친구가 되었어요!"
         }
