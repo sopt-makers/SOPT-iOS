614bb2873078a74c1e13130ea863808ce0d54d26
Sejin Lee <lsj8706@naver.com>
2023-12-31 20:06:15 +0900

    [Test] #346 - NotificationPayload 테스트

---
+++ SOPT-iOS/Projects/Features/RootFeature/Tests/Sources/NotificationPayloadTests.swift
@@ -0,0 +1,112 @@
+//
+//  NotificationPayloadTests.swift
+//  RootFeatureTests
+//
+//  Created by sejin on 12/31/23.
+//  Copyright © 2023 SOPT-iOS. All rights reserved.
+//
+
+import XCTest
+
+@testable import RootFeature
+
+final class NotificationPayloadTests: XCTestCase {
+    private var payload: NotificationPayload!
+    
+    override func setUp() {
+        super.setUp()
+    }
+    
+    override func tearDown() {
+        super.tearDown()
+        self.payload = nil
+    }
+    
+    private func loadMockPayload(resource: String) {
+        guard let path = Bundle(for: NotificationPayloadTests.self).path(forResource: resource, ofType: "json")  else { return }
+        
+        guard let jsonString = try? String(contentsOfFile: path) else {
+            return
+        }
+        
+        if let jsonData = jsonString.data(using: .utf8) {
+            do {
+                if let jsonDict = try JSONSerialization.jsonObject(with: jsonData, options: []) as? [AnyHashable: Any] {
+                    self.payload = NotificationPayload(dictionary: jsonDict)
+                }
+            } catch {
+                print("Error parsing JSON: \(error)")
+            }
+        }
+    }
+    
+    func test_payload_decoding() {
+        // Given
+        let mockJson = "MockDeepLinkPayload"
+        
+        // When
+        self.loadMockPayload(resource: mockJson)
+        
+        // Then
+        XCTAssertEqual(payload.id, "2133")
+        XCTAssertEqual(payload.deepLink, "home/poke/notification-list")
+        XCTAssertEqual(payload.category, "NOTICE")
+        XCTAssertEqual(payload.aps.alert.title, "테스트")
+        XCTAssertEqual(payload.aps.alert.body, "안녕하세요")
+    }
+    
+    func test_hasLink() {
+        // Given
+        let mockJson = "MockDeepLinkPayload"
+
+        // When
+        self.loadMockPayload(resource: mockJson)
+        
+        // Then
+        XCTAssertTrue(payload.hasLink)
+    }
+    
+    func test_hasDeepLink_딥링크만_있는_경우() {
+        // Given
+        let mockJson = "MockDeepLinkPayload"
+
+        // When
+        self.loadMockPayload(resource: mockJson)
+        
+        // Then
+        XCTAssertTrue(payload.hasDeepLink)
+    }
+    
+    func test_hasDeepLink_웹링크만_있는_경우() {
+        // Given
+        let mockJson = "MockWebLinkPayload"
+
+        // When
+        self.loadMockPayload(resource: mockJson)
+        
+        // Then
+        XCTAssertFalse(payload.hasDeepLink)
+    }
+    
+    func test_hasWebLink_딥링크만_있는_경우() {
+        // Given
+        let mockJson = "MockDeepLinkPayload"
+
+        // When
+        self.loadMockPayload(resource: mockJson)
+        
+        // Then
+        XCTAssertFalse(payload.hasWebLink)
+    }
+    
+    func test_hasWebLink_웹링크만_있는_경우() {
+        // Given
+        let mockJson = "MockWebLinkPayload"
+
+        // When
+        self.loadMockPayload(resource: mockJson)
+        
+        // Then
+        XCTAssertTrue(payload.hasWebLink)
+    }
+}
