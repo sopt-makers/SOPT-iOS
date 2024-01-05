f00a0f1f23e53aeb8d256c8e0c0515b04f381836
Sejin Lee <lsj8706@naver.com>
2024-01-05 13:38:19 +0900

    [Test] #346 - DeepLinkParserTests 테스트 메서드 이름 수정

--- SOPT-iOS/Projects/Features/RootFeature/Tests/Sources/DeepLinkParserTests.swift
+++ SOPT-iOS/Projects/Features/RootFeature/Tests/Sources/DeepLinkParserTests.swift
@@ -24,7 +24,7 @@ final class DeepLinkParserTests: XCTestCase {
         self.parser = nil
     }
     
-    func test_URL_형태의_링크가_아닌_경우() {
+    func test_URL_형태의_링크가_아닌_경우를_파싱한다() {
         // Given
         let link = "FAIL https:///test/url"
         let expected: DeepLinkData = ([HomeDeepLink()], nil)
@@ -37,7 +37,7 @@ final class DeepLinkParserTests: XCTestCase {
         XCTAssertNil(result.queryItems)
     }
     
-    func test_만료된_링크() {
+    func test_만료된_딥링크를_파싱한다() {
         // Given
         let link = "home?expiredAt=2023-01-01T00:00:00.000Z"
         
@@ -52,7 +52,7 @@ final class DeepLinkParserTests: XCTestCase {
         }
     }
     
-    func test_쿼리_1개_파싱() {
+    func test_쿼리_1개를_파싱한다() {
         // Given
         let link = "home?id=123"
         
@@ -64,7 +64,7 @@ final class DeepLinkParserTests: XCTestCase {
         XCTAssertTrue(result.queryItems?.getQueryValue(key: "id") == "123")
     }
     
-    func test_쿼리_2개_파싱() {
+    func test_쿼리_2개를_파싱한다() {
         // Given
         let link = "home?id=123&status=active"
         
@@ -77,7 +77,7 @@ final class DeepLinkParserTests: XCTestCase {
         XCTAssertTrue(result.queryItems?.getQueryValue(key: "status") == "active")
     }
     
-    func test_잘못된_루트_딥링크인_경우() {
+    func test_잘못된_루트_딥링크인_경우를_파싱한다() {
         // Given
         let link = "none"
         
@@ -92,7 +92,7 @@ final class DeepLinkParserTests: XCTestCase {
         }
     }
     
-    func test_정의되지_않은_path인_경우() {
+    func test_정의되지_않은_path인_경우를_파싱한다() {
         // Given
         let link = "home/none"
         
@@ -107,7 +107,7 @@ final class DeepLinkParserTests: XCTestCase {
         }
     }
     
-    func test_알림_리스트_딥링크() {
+    func test_알림_리스트_딥링크를_파싱한다() {
         // Given
         let link = "home/notification"
         let expected = ["home", "notification"]
@@ -120,7 +120,7 @@ final class DeepLinkParserTests: XCTestCase {
         XCTAssertEqual(deepLinks, expected)
     }
     
-    func test_알림_디테일_딥링크() {
+    func test_알림_디테일_딥링크를_파싱한다() {
         // Given
         let link = "home/notification/detail?id=1234"
         let expected = ["home", "notification", "detail"]
@@ -135,7 +135,7 @@ final class DeepLinkParserTests: XCTestCase {
         XCTAssertTrue(result.queryItems?.getQueryValue(key: "id") == "1234")
     }
     
-    func test_마이페이지_딥링크() {
+    func test_마이페이지_딥링크를_파싱한다() {
         // Given
         let link = "home/mypage"
         let expected = ["home", "mypage"]
@@ -148,7 +148,7 @@ final class DeepLinkParserTests: XCTestCase {
         XCTAssertEqual(deepLinks, expected)
     }
     
-    func test_출석_딥링크() {
+    func test_출석_딥링크를_파싱한다() {
         // Given
         let link = "home/attendance"
         let expected = ["home", "attendance"]
@@ -161,7 +161,7 @@ final class DeepLinkParserTests: XCTestCase {
         XCTAssertEqual(deepLinks, expected)
     }
     
-    func test_출석하기_모달_딥링크() {
+    func test_출석하기_모달_딥링크를_파싱한다() {
         // Given
         let link = "home/attendance/attendance-modal?subLecturedId=1&round=2"
         let expected = ["home", "attendance", "attendance-modal"]
@@ -174,7 +174,7 @@ final class DeepLinkParserTests: XCTestCase {
         XCTAssertEqual(deepLinks, expected)
     }
     
-    func test_솝탬프_전체미션_딥링크() {
+    func test_솝탬프_전체미션_딥링크를_파싱한다() {
         // Given
         let link = "home/soptamp"
         let expected = ["home", "soptamp"]
@@ -187,7 +187,7 @@ final class DeepLinkParserTests: XCTestCase {
         XCTAssertEqual(deepLinks, expected)
     }
     
-    func test_솝탬프_전체랭킹_딥링크() {
+    func test_솝탬프_전체랭킹_딥링크를_파싱한다() {
         // Given
         let link = "home/soptamp/entire-ranking"
         let expected = ["home", "soptamp", "entire-ranking"]
@@ -200,7 +200,20 @@ final class DeepLinkParserTests: XCTestCase {
         XCTAssertEqual(deepLinks, expected)
     }
     
-    func test_솝탬프_현재기수_랭킹_딥링크() {
+    func test_콕찌르기_알림리스트_딥링크를_파싱한다() {
+        // Given
+        let link = "home/poke/notification-list"
+        let expected = ["home", "poke", "notification-list"]
+        
+        // When
+        guard let result = try? parser.parse(with: link) else { return XCTFail() }
+        let deepLinks = result.deepLinks.map { $0.name }
+
+        // Then
+        XCTAssertEqual(deepLinks, expected)
+    }
+    
+    func test_솝탬프_현재기수_랭킹_딥링크를_파싱한다() {
         // Given
         let link = "home/soptamp/current-generation-ranking?currentGeneration=33&status=active"
         let expected = ["home", "soptamp", "current-generation-ranking"]
@@ -217,7 +230,7 @@ final class DeepLinkParserTests: XCTestCase {
         XCTAssertEqual(queryItems?.getQueryValue(key: "status"), "active")
     }
     
-    func test_딥링크_파싱_종착지뷰_설정() {
+    func test_딥링크_파싱_종착지뷰_설정을_파싱한다() {
         // Given
         let link = "home/notification"
         let expected = true
@@ -235,7 +248,7 @@ final class DeepLinkParserTests: XCTestCase {
         XCTAssertEqual(expected, lastDeepLink.isDestination)
     }
     
-    func test_딥링크_파싱_경유지뷰_설정() {
+    func test_딥링크_파싱_경유지뷰_설정을_파싱한다() {
         // Given
         let link = "home/notification"
         let expected = false
