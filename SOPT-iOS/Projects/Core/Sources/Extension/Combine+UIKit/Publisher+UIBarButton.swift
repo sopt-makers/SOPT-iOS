6e0c23d988f368c5638dd2bd8bec7615e0728e96
Ian <elesahich@gmail.com>
2024-04-11 01:05:39 +0900

    [Fix] 민재 리뷰 반영: 미사용 Extension 제거

--- SOPT-iOS/Projects/Core/Sources/Extension/Combine+UIKit/Publisher+UIBarButton.swift
+++ SOPT-iOS/Projects/Core/Sources/Extension/Combine+UIKit/Publisher+UIBarButton.swift
@@ -67,59 +67,10 @@ public extension UIBarButtonItem {
 
 // MARK: - CombineCompatible
 
-extension UIBarButtonItem: CombineCompatible {}
+extension UIBarButtonItem: CombineCompatible { }
 
 extension CombineCompatible where Self: UIBarButtonItem {
   public var tapPublisher: UIBarButtonItem.Publisher<Self> {
     return UIBarButtonItem.Publisher(output: self)
-  }
-}
-
-
-
-import Combine
-import UIKit
-
-extension UIControl {
-  func controlPublisher(for event: UIControl.Event) -> UIControl.EventPublisher {
-    return UIControl.EventPublisher(control: self, event: event)
-  }
-  
-  struct EventPublisher: Publisher {
-    typealias Output = UIControl
-    typealias Failure = Never
-    
-    let control: UIControl
-    let event: UIControl.Event
-    
-    func receive<S>(subscriber: S) where S: Subscriber, Never == S.Failure, UIControl == S.Input {
-      let subscription = EventSubscription(control: control, subscrier: subscriber, event: event)
-      subscriber.receive(subscription: subscription)
-    }
-  }
-  
-  fileprivate class EventSubscription<EventSubscriber: Subscriber>: Subscription where EventSubscriber.Input == UIControl, EventSubscriber.Failure == Never {
-    let control: UIControl
-    let event: UIControl.Event
-    var subscriber: EventSubscriber?
-    
-    init(control: UIControl, subscrier: EventSubscriber, event: UIControl.Event) {
-      self.control = control
-      self.subscriber = subscrier
-      self.event = event
-      
-      control.addTarget(self, action: #selector(eventDidOccur), for: event)
-    }
-    
-    func request(_ demand: Subscribers.Demand) { }
-    
-    func cancel() {
-      subscriber = nil
-      control.removeTarget(self, action: #selector(eventDidOccur), for: event)
-    }
-    
-    @objc func eventDidOccur() {
-      _ = subscriber?.receive(control)
-    }
   }
 }
