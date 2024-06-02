8f8ef9efaeae3156ab4ef509862e51773f99804f
Ian <elesahich@gmail.com>
2024-06-02 14:38:43 +0900

    [Etc] 주석 처리 추가

--- SOPT-iOS/Projects/Modules/Networks/Sources/Service/PokeService.swift
+++ SOPT-iOS/Projects/Modules/Networks/Sources/Service/PokeService.swift
@@ -27,6 +27,7 @@ public protocol PokeService {
 }
 
 extension PokeService {
+  // TODO(@승호): 컴파일 되도록 임시 처리 합니다. 작업시에 수정해 주세요~
   public func poke(userId: Int, message: String, isAnonymous: Bool = false) -> AnyPublisher<PokeUserEntity, Error> {
     self.poke(userId: userId, message: message, isAnonymous: isAnonymous)
   }
