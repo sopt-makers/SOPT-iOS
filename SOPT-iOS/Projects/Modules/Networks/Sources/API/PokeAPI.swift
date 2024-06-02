0edf0892f04f7a496effc348f1fbd2899330e8fe
lidum (ian) <elesahich@gmail.com>
2024-06-02 15:07:23 +0900

    [Feature] 모델, 네트워크 변경점 반영  (#371)

--- SOPT-iOS/Projects/Modules/Networks/Sources/API/PokeAPI.swift
+++ SOPT-iOS/Projects/Modules/Networks/Sources/API/PokeAPI.swift
@@ -13,66 +13,68 @@ import Moya
 import Core
 
 public enum PokeAPI {
-    case isNewUser
-    case getWhoPokedToMe
-    case getWhoPokedToMeList(pageIndex: String)
-    case getFriend
-    case getFriendRandomUser
-    case getFriendList
-    case getFriendListWithRelation(relation: String, page: Int)
-    case getRandomUsers
-    case getPokeMessages(messageType: String)
-    case poke(userId: String, params: Parameters)
+  case isNewUser
+  case getWhoPokedToMe
+  case getWhoPokedToMeList(pageIndex: String)
+  case getFriend
+  case getFriendRandomUser
+  case getFriendList
+  case getFriendListWithRelation(relation: String, page: Int)
+  case getRandomUsers(params: Parameters)
+  case getPokeMessages(messageType: String)
+  case poke(userId: String, params: Parameters)
 }
 
 extension PokeAPI: BaseAPI {
-    public static var apiType: APIType = .poke
-    
-    public var path: String {
-        switch self {
-        case .isNewUser:
-            return "/new"
-        case .getWhoPokedToMe:
-            return "/to/me"
-        case .getWhoPokedToMeList:
-            return "/to/me/list"
-        case .getFriend:
-            return "/friend"
-        case .getFriendRandomUser:
-            return "/friend/random-user"
-        case .getFriendList, .getFriendListWithRelation:
-            return "/friend/list"
-        case .getRandomUsers:
-            return "/random-user"
-        case .getPokeMessages:
-            return "/message"
-        case .poke(let userId, _):
-            return "/\(userId)"
-        }
+  public static var apiType: APIType = .poke
+  
+  public var path: String {
+    switch self {
+    case .isNewUser:
+      return "/new"
+    case .getWhoPokedToMe:
+      return "/to/me"
+    case .getWhoPokedToMeList:
+      return "/to/me/list"
+    case .getFriend:
+      return "/friend"
+    case .getFriendRandomUser:
+      return "/friend/random-user"
+    case .getFriendList, .getFriendListWithRelation:
+      return "/friend/list"
+    case .getRandomUsers:
+      return "/random-user"
+    case .getPokeMessages:
+      return "/message"
+    case .poke(let userId, _):
+      return "/\(userId)"
     }
-    
-    public var method: Moya.Method {
-        switch self {
-        case .isNewUser, .getWhoPokedToMe, .getWhoPokedToMeList, .getFriend, .getFriendListWithRelation, .getFriendRandomUser,
-                .getFriendList, .getRandomUsers, .getPokeMessages:
-            return .get
-        case .poke:
-            return .put
-        }
+  }
+  
+  public var method: Moya.Method {
+    switch self {
+    case .isNewUser, .getWhoPokedToMe, .getWhoPokedToMeList, .getFriend, .getFriendListWithRelation, .getFriendRandomUser,
+        .getFriendList, .getRandomUsers, .getPokeMessages:
+      return .get
+    case .poke:
+      return .put
     }
-    
-    public var task: Moya.Task {
-        switch self {
-        case .getWhoPokedToMeList(let pageIndex):
-            return .requestParameters(parameters: ["page": pageIndex], encoding: URLEncoding.queryString)
-        case .getPokeMessages(let messageType):
-            return .requestParameters(parameters: ["messageType": messageType], encoding: URLEncoding.queryString)
-        case .getFriendListWithRelation(let relation, let page):
-            return .requestParameters(parameters: ["type": relation, "page": page], encoding: URLEncoding.queryString)
-        case .poke(_, let params):
-            return .requestParameters(parameters: params, encoding: JSONEncoding.default)
-        default:
-            return .requestPlain
-        }
+  }
+  
+  public var task: Moya.Task {
+    switch self {
+    case .getWhoPokedToMeList(let pageIndex):
+      return .requestParameters(parameters: ["page": pageIndex], encoding: URLEncoding.queryString)
+    case .getPokeMessages(let messageType):
+      return .requestParameters(parameters: ["messageType": messageType], encoding: URLEncoding.queryString)
+    case .getFriendListWithRelation(let relation, let page):
+      return .requestParameters(parameters: ["type": relation, "page": page], encoding: URLEncoding.queryString)
+    case .poke(_, let params):
+      return .requestParameters(parameters: params, encoding: JSONEncoding.default)
+    case .getRandomUsers(let params):
+      return .requestParameters(parameters: params, encoding: URLEncoding.queryString)
+    default:
+      return .requestPlain
     }
+  }
 }
--- SOPT-iOS/Projects/Modules/Networks/Sources/Entity/PokeUserEntity.swift
+++ SOPT-iOS/Projects/Modules/Networks/Sources/Entity/PokeUserEntity.swift
@@ -9,39 +9,44 @@
 import Foundation
 
 public struct PokeUserEntity {
-    public let userId: Int
-    public let playgroundId: Int
-    public let profileImage: String
-    public let name: String
-    public let generation: Int
-    public let part: String
-    public let pokeNum: Int
-    public let message: String
-    public let relationName: String
-    public let mutualRelationMessage: String
-    public let isFirstMeet, isAlreadyPoke: Bool
+  public let userId: Int
+  public let playgroundId: Int
+  public let profileImage: String
+  public let name: String
+  public let generation: Int
+  public let part: String
+  public let pokeNum: Int
+  public let message: String
+  public let relationName: String
+  public let mutualRelationMessage: String
+  public let isFirstMeet, isAlreadyPoke: Bool
+  public let isAnonymous: Bool
+  public let anonymousName: String
 }
 
 extension PokeUserEntity: Codable {
-    public enum CodingKeys: CodingKey {
-        case userId, playgroundId, profileImage, name, generation, part, pokeNum,
-         message, relationName, mutualRelationMessage, isFirstMeet, isAlreadyPoke
-    }
+  public enum CodingKeys: CodingKey {
+    case userId, playgroundId, profileImage, name, generation, part, pokeNum,
+         message, relationName, mutualRelationMessage, isFirstMeet, isAlreadyPoke,
+         isAnonymous, anonymousName
+  }
+  
+  public init(from decoder: Decoder) throws {
+    let container = try decoder.container(keyedBy: CodingKeys.self)
     
-    public init(from decoder: Decoder) throws {
-        let container = try decoder.container(keyedBy: CodingKeys.self)
-        
-        self.userId = try container.decode(Int.self, forKey: .userId)
-        self.playgroundId = try container.decode(Int.self, forKey: .playgroundId)
-        self.profileImage = try container.decodeIfPresent(String.self, forKey: .profileImage) ?? ""
-        self.name = try container.decode(String.self, forKey: .name)
-        self.generation = try container.decode(Int.self, forKey: .generation)
-        self.part = try container.decode(String.self, forKey: .part)
-        self.pokeNum = try container.decode(Int.self, forKey: .pokeNum)
-        self.message = try container.decode(String.self, forKey: .message)
-        self.relationName = try container.decode(String.self, forKey: .relationName)
-        self.mutualRelationMessage = try container.decode(String.self, forKey: .mutualRelationMessage)
-        self.isFirstMeet = try container.decode(Bool.self, forKey: .isFirstMeet)
-        self.isAlreadyPoke = try container.decode(Bool.self, forKey: .isAlreadyPoke)
-    }
+    self.userId = try container.decode(Int.self, forKey: .userId)
+    self.playgroundId = try container.decode(Int.self, forKey: .playgroundId)
+    self.profileImage = try container.decodeIfPresent(String.self, forKey: .profileImage) ?? ""
+    self.name = try container.decode(String.self, forKey: .name)
+    self.generation = try container.decode(Int.self, forKey: .generation)
+    self.part = try container.decode(String.self, forKey: .part)
+    self.pokeNum = try container.decode(Int.self, forKey: .pokeNum)
+    self.message = try container.decode(String.self, forKey: .message)
+    self.relationName = try container.decode(String.self, forKey: .relationName)
+    self.mutualRelationMessage = try container.decode(String.self, forKey: .mutualRelationMessage)
+    self.isFirstMeet = try container.decode(Bool.self, forKey: .isFirstMeet)
+    self.isAlreadyPoke = try container.decode(Bool.self, forKey: .isAlreadyPoke)
+    self.isAnonymous = try container.decode(Bool.self, forKey: .isAnonymous)
+    self.anonymousName = try container.decode(String.self, forKey: .anonymousName)
+  }
 }
