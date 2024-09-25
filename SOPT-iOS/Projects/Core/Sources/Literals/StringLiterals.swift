6dcb25dae8f743d5cf2853194b21d6fbaf94e68c
yungu0010 <dbstj16@sookmyung.ac.kr>
2024-09-26 03:21:34 +0900

    [Feat] #383 - 솝마디 메인 뷰 구현

--- SOPT-iOS/Projects/Core/Sources/Literals/StringLiterals.swift
+++ SOPT-iOS/Projects/Core/Sources/Literals/StringLiterals.swift
@@ -372,6 +372,8 @@ public struct I18N {
         public static let poke = "콕 찌르기"
         public static let pokeFortunatePerson = "행운이 2배가 될 솝트인을 찔러보세요"
         public static let receiveTodaysTalisman = "오늘의 부적 받기"
+		public static let recieveTodayFortune = "오늘의 운세가 도착했어요!"
+		public static let checkTodayFortune = "오늘의 운세 확인하기"
     }
 }
 
