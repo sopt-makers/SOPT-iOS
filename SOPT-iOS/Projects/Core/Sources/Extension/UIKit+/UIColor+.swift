e2a0dabc870c780ed76e719f549bb45238543a36
dlwogus0128 <79050615+dlwogus0128@users.noreply.github.com>
2024-09-28 01:34:10 +0900

    [Feat] #392 - Color hex 표현 Extension 추가

--- SOPT-iOS/Projects/Core/Sources/Extension/UIKit+/UIColor+.swift
+++ SOPT-iOS/Projects/Core/Sources/Extension/UIKit+/UIColor+.swift
@@ -24,4 +24,18 @@ public extension UIColor {
            blue: rgb & 0xFF
        )
    }
+
+    convenience init(hex: String) {
+        let scanner = Scanner(string: hex)
+        _ = scanner.scanString("#")
+        
+        var rgb: UInt64 = 0
+        scanner.scanHexInt64(&rgb)
+        
+        let r = CGFloat((rgb >> 16) & 0xFF) / 255.0
+        let g = CGFloat((rgb >> 8) & 0xFF) / 255.0
+        let b = CGFloat((rgb >> 0) & 0xFF) / 255.0
+        
+        self.init(red: r, green: g, blue: b, alpha: 1.0)
+    }
 }
