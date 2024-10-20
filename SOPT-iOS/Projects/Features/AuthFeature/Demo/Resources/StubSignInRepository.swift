3e97abd2d179d054d0b5b5b0ef9ba3255a77d084
Melt <seokwoo2000@naver.com>
2024-10-20 17:59:30 +0900

    [Feat] AuthDemo앱 AppDelegate, SceneDelegate 구현

---
+++ SOPT-iOS/Projects/Features/AuthFeature/Demo/Resources/StubSignInRepository.swift
@@ -0,0 +1,20 @@
+//
+//  StubUserService.swift
+//  AuthFeatureTests
+//
+//  Created by 장석우 on 10/20/24.
+//  Copyright © 2024 SOPT-iOS. All rights reserved.
+//
+
+import Combine
+import Foundation
+
+import Domain
+
+struct StubSignInRepository: SignInRepositoryInterface {
+    func requestSignIn(token: String) -> AnyPublisher<Bool, Error> {
+        Just(true).setFailureType(to: Error.self).eraseToAnyPublisher()
+    }
+    
+    
+}
--- SOPT-iOS/Projects/Features/AuthFeature/Demo/Sources/Demo.swift
+++ SOPT-iOS/Projects/Features/AuthFeature/Demo/Sources/Demo.swift
@@ -6,4 +6,77 @@
 //  Copyright © 2023 SOPT-iOS. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+
+import BaseFeatureDependency
+
+import AuthFeature
+import AuthFeatureInterface
+
+import Domain
+import Core
+
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    func application( _ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        DIContainer.shared.register(
+            interface: SignInRepositoryInterface.self,
+            implement: { StubSignInRepository()}
+        )
+        
+        return true
+    }
+    
+    // MARK: UISceneSession Lifecycle
+    
+    func application( _ application: UIApplication,
+        configurationForConnecting connectingSceneSession: UISceneSession,
+        options: UIScene.ConnectionOptions
+    ) -> UISceneConfiguration {
+        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+    }
+    
+    func application( _ application: UIApplication,
+        didDiscardSceneSessions sceneSessions: Set<UISceneSession>
+    ) {}
+}
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+    
+    var window: UIWindow?
+    
+    var rootController: UINavigationController {
+        return self.window!.rootViewController as? UINavigationController ?? UINavigationController(rootViewController: UIViewController())
+    }
+    
+    lazy var authCoordinator: AuthCoordinator = AuthCoordinator(router: Router(rootController: rootController), factory: AuthBuilder())
+    
+    func scene(_ scene: UIScene,
+               willConnectTo session: UISceneSession,
+               options connectionOptions: UIScene.ConnectionOptions) {
+        guard let scene = (scene as? UIWindowScene) else { return }
+        let vc = UIViewController()
+        vc.view.backgroundColor = .purple
+        window = UIWindow(windowScene: scene)
+        window?.rootViewController = vc
+        window?.makeKeyAndVisible()
+        
+        self.authCoordinator.start()
+    }
+
+    func sceneDidDisconnect(_ scene: UIScene) {}
+
+    func sceneDidBecomeActive(_ scene: UIScene) {
+        UIApplication.shared.applicationIconBadgeNumber = 0
+    }
+
+    func sceneWillResignActive(_ scene: UIScene) {}
+
+    func sceneWillEnterForeground(_ scene: UIScene) {}
+
+    func sceneDidEnterBackground(_ scene: UIScene) {}
+}
