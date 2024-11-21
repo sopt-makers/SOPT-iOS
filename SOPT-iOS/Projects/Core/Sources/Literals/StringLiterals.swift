6e99e802da1978076d3e4fc3f460da13100d1d43
dlwogus0128 <79050615+dlwogus0128@users.noreply.github.com>
2024-11-22 02:57:45 +0900

    [Feat] #438 - 메인 섹션 관련 StringLiterals 추가

--- SOPT-iOS/Projects/Core/Sources/Literals/StringLiterals.swift
+++ SOPT-iOS/Projects/Core/Sources/Literals/StringLiterals.swift
@@ -226,6 +226,40 @@ public struct I18N {
         }
     }
     
+    public struct Home {
+        public struct MainService {
+            public struct UserHistory {
+                public static let visitor = "비회원"
+                public static let active = "기 활동 중"
+                public static let inactive = "기 수료"
+                public static let inactiveMember = "비활동"
+                public static let encourage = "안녕하세요.\nSOPT의 열정이 되어주세요!"
+                public static let welcome = "안녕하세요.\nSOPT에 오신 것을 환영합니다!"
+            }
+            
+            public struct Attendance {
+                public static let attendance = "출석"
+            }
+            
+            public struct Services {
+                public static let playground = "Playground"
+                public static let groupAndStudy = "모임/스터디"
+                public static let member = "멤버"
+                public static let project = "프로젝트"
+            }
+        }
+        
+        public struct AppService { }
+        
+        public struct Insight { }
+        
+        public struct Group { }
+        
+        public struct CoffeeChat { }
+        
+        public struct Announcements { }
+    }
+    
     public struct Attendance {
         public static func nthAttendance(_ idx: Int) -> String {
             return "\(idx)차 출석"
