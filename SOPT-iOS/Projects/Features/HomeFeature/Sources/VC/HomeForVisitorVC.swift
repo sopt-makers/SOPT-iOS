5e3b328b5ce5c466c11e74be5caf1356f8de85f0
dlwogus0128 <79050615+dlwogus0128@users.noreply.github.com>
2024-11-21 17:33:42 +0900

    [Feat] #438 - Sources 하위 폴더링 및 홈뷰 멤버 / 방문자 VC 파일 생성

---
+++ SOPT-iOS/Projects/Features/HomeFeature/Sources/VC/HomeForVisitorVC.swift
@@ -0,0 +1,30 @@
+//
+//  HomeForVisitorVC.swift
+//  HomeFeature
+//
+//  Created by Jae Hyun Lee on 11/21/24.
+//  Copyright © 2024 SOPT-iOS. All rights reserved.
+//
+
+import UIKit
+
+class HomeForVisitorVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
