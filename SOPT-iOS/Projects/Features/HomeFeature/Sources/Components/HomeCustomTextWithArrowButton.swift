4897119d6fb5d1d2ea0b0d09a410962976967874
dlwogus0128 <79050615+dlwogus0128@users.noreply.github.com>
2024-11-21 17:08:12 +0900

    [Fix] #434 - 코드 리뷰 반영

--- SOPT-iOS/Projects/Features/HomeFeature/Sources/Components/HomeCustomTextWithArrowButton.swift
+++ SOPT-iOS/Projects/Features/HomeFeature/Sources/Components/HomeCustomTextWithArrowButton.swift
@@ -31,39 +31,24 @@ extension HomeCustomTextWithArrowButton {
     private func setUI(_ title: String) {
         self.setBackgroundColor(.clear, for: .normal)
         
-        /// 텍스트 지정
-        let font = DSKitFontFamily.Suit.semiBold.font(size: 12)
-        let foregroundColor = DSKitAsset.Colors.gray300.color
+        self.configuration = UIButton.Configuration.plain()
 
-        let attributedText = NSAttributedString(
-            string: title,
-            attributes: [
-                .font: font,
-                .foregroundColor: foregroundColor
-            ]
-        )
-        
-        /// 이미지 지정
-        let imageAttachment = NSTextAttachment()
-        imageAttachment.image = DSKitAsset.Assets.chevronRight.image.withTintColor(DSKitAsset.Colors.gray300.color)
+        self.configurationUpdateHandler = { button in
+            guard var configuration = button.configuration else { return }
 
-        let imageSize = CGSize(width: 16, height: 16)
-        imageAttachment.bounds = CGRect(x: 0,
-                                        y: (font.lineHeight - imageSize.height) / 2 + font.descender,
-                                        width: imageSize.width,
-                                        height: imageSize.height)
-        
-        let attributedImage = NSAttributedString(attachment: imageAttachment)
-        
-        /// 텍스트 + 이미지 결합
-        let combinedAttributedString = NSMutableAttributedString()
-        combinedAttributedString.append(attributedText)
-        combinedAttributedString.append(attributedImage)
-        
-        /// 타이틀 지정
-        self.setAttributedTitle(
-            combinedAttributedString,
-            for: .normal
-        )
+            /// 타이틀 설정
+            var attributedTitle = AttributedString(title)
+            var attributes = AttributeContainer()
+            attributes.font = DSKitFontFamily.Suit.semiBold.font(size: 12)
+            attributes.foregroundColor = DSKitAsset.Colors.gray300.color
+            attributedTitle.setAttributes(attributes)
+            configuration.attributedTitle = attributedTitle
+            
+            /// 이미지 설정
+            configuration.image = DSKitAsset.Assets.chevronRight.image.withTintColor(DSKitAsset.Colors.gray300.color)
+            configuration.imagePlacement = .trailing
+            
+            button.configuration = configuration
+        }
     }
 }
