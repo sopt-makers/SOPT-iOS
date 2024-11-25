e33bd47470d6f136e982ce2c4277347930b332a0
yungu0010 <dbstj16@sookmyung.ac.kr>
2024-11-25 23:51:58 +0900

    [Feat] #442 - vc, vm 생성

--- SOPT-iOS/Projects/Features/SoptlogFeature/Sources/SoptlogScene/VC/SoptlogVC.swift
+++ SOPT-iOS/Projects/Features/SoptlogFeature/Sources/SoptlogScene/VC/SoptlogVC.swift
@@ -8,23 +8,55 @@
 
 import UIKit
 
-class SoptlogVC: UIViewController {
+import Core
+import Domain
+import DSKit
+
+import BaseFeatureDependency
+
+final class SoptlogVC: UIViewController, SoptlogViewControllable {
+    
+    // MARK: - Properties
+
+    public let viewModel: SoptlogViewModel
+    
+    // MARK: - UI Components
+    
+    // MARK: - Initialization
+    
+    public init(viewModel: SoptlogViewModel) {
+        self.viewModel = viewModel
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
+    // MARK: - View Life Cycle
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        setUI()
+        setLayout()
+    }
+}
 
-        // Do any additional setup after loading the view.
+// MARK: - UI & Layout
+
+extension SoptlogVC {
+    private func setUI() {
+        self.navigationController?.isNavigationBarHidden = true
+        view.backgroundColor = DSKitAsset.Colors.semanticBackground.color
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    private func setLayout() {
     }
-    */
+}
 
+// MARK: - Methods
+
+extension SoptlogVC {
+    
 }
--- SOPT-iOS/Projects/Features/SoptlogFeature/Sources/SoptlogScene/ViewModel/SoptlogViewModel.swift
+++ SOPT-iOS/Projects/Features/SoptlogFeature/Sources/SoptlogScene/ViewModel/SoptlogViewModel.swift
@@ -7,3 +7,33 @@
 //
 
 import Foundation
+import Combine
+
+import Core
+import Domain
+
+import HomeFeatureInterface
+import BaseFeatureDependency
+
+public class SoptlogViewModel: SoptlogViewModelType {
+    
+    // MARK: - Inputs
+    
+    public struct Input { }
+    
+    // MARK: - Outputs
+    
+    public struct Output { }
+    
+    // MARK: - initialization
+    
+    public init() { }
+}
+
+extension SoptlogViewModel {
+    public func transform(from input: Input, cancelBag: CancelBag) -> Output {
+        let output = Output()
+        return output
+    }
+}
+
