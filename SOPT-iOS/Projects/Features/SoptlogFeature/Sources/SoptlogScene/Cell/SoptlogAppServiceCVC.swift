04ec28170bf41ed67382a09433f62cf894d3e6a9
yungu0010 <dbstj16@sookmyung.ac.kr>
2024-11-26 03:00:43 +0900

    [Feat] #442 - appServiceCell 구현

--- SOPT-iOS/Projects/Features/SoptlogFeature/Sources/SoptlogScene/Cell/SoptlogAppServiceCVC.swift
+++ SOPT-iOS/Projects/Features/SoptlogFeature/Sources/SoptlogScene/Cell/SoptlogAppServiceCVC.swift
@@ -8,6 +8,67 @@
 
 import UIKit
 
-class SoptlogAppServiceCVC: UICollectionViewCell {
+import Core
+import DSKit
+
+final class SoptlogAppServiceCVC: UICollectionViewCell {
     
+    // MARK: - UI Components
+
+    private let titleLabel = UILabel().then {
+        $0.text = "솝레벨"
+        $0.textColor = DSKitAsset.Colors.gray200.color
+        $0.font = DSKitFontFamily.Suit.medium.font(size: 14)
+    }
+    
+    private let serviceImageView = UIImageView().then {
+        $0.image = DSKitAsset.Assets.soptampLogo.image
+    }
+    
+    private let serviceLabel = UILabel().then {
+        $0.text = "Lv.6"
+        $0.textColor = DSKitAsset.Colors.white.color
+        $0.font = DSKitFontFamily.Suit.bold.font(size: 16)
+    }
+    
+    // MARK: - init
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setUI()
+        setLayout()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+extension SoptlogAppServiceCVC {
+    
+    private func setUI() {
+        contentView.backgroundColor = .clear
+    }
+    
+    private func setLayout() {
+        contentView.addSubviews(titleLabel, serviceImageView, serviceLabel)
+        
+        titleLabel.snp.makeConstraints { make in
+            make.centerX.top.equalToSuperview()
+        }
+        
+        serviceImageView.snp.makeConstraints { make in
+            make.top.equalTo(titleLabel.snp.bottom).offset(6)
+        }
+        
+        serviceLabel.snp.makeConstraints { make in
+            make.top.equalTo(serviceImageView.snp.bottom).offset(4)
+            make.centerX.bottom.equalToSuperview()
+        }
+    }
+    
+    func configureCell(title: String, image: UIImage) {
+        self.titleLabel.text = title
+        self.serviceImageView.image = image
+    }
 }
