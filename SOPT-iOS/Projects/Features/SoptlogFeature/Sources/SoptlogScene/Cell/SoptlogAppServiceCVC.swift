b82df4ba9223ae692f341e00c5a78c99bc1020a5
yungu0010 <dbstj16@sookmyung.ac.kr>
2024-11-26 03:00:13 +0900

    [Feat] #442 - soptlog 헤더뷰 구현

---
+++ SOPT-iOS/Projects/Features/SoptlogFeature/Sources/SoptlogScene/Cell/SoptlogAppServiceCVC.swift
@@ -0,0 +1,13 @@
+//
+//  SoptlogAppServiceCVC.swift
+//  SoptlogFeature
+//
+//  Created by 강윤서 on 11/26/24.
+//  Copyright © 2024 SOPT-iOS. All rights reserved.
+//
+
+import UIKit
+
+class SoptlogAppServiceCVC: UICollectionViewCell {
+    
+}
