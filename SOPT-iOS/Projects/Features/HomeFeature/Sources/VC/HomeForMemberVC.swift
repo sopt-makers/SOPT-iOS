3b35ae39b05dd1667925055c36dacd76720c092a
dlwogus0128 <79050615+dlwogus0128@users.noreply.github.com>
2024-11-28 03:38:08 +0900

    [Add] #452 - 커피솝 셀 Register

--- SOPT-iOS/Projects/Features/HomeFeature/Sources/VC/HomeForMemberVC.swift
+++ SOPT-iOS/Projects/Features/HomeFeature/Sources/VC/HomeForMemberVC.swift
@@ -108,6 +108,10 @@ extension HomeForMemberVC {
                                      forCellWithReuseIdentifier: AppServiceCardCVC.className)
         self.collectionView.register(InsightCardCVC.self,
                                      forCellWithReuseIdentifier: InsightCardCVC.className)
+        self.collectionView.register(GroupCardCVC.self,
+                                     forCellWithReuseIdentifier: GroupCardCVC.className)
+        self.collectionView.register(CoffeeChatCardCVC.self,
+                                     forCellWithReuseIdentifier: CoffeeChatCardCVC.className)
     }
 }
 
