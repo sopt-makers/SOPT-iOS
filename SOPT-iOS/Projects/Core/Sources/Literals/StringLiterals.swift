cc62db75699ff62494ded1b02f1e3dfa4cc07d9f
yungu0010 <dbstj16@sookmyung.ac.kr>
2024-11-29 05:37:06 +0900

    [Fix] #442 - 한 줄 소개 기획 변경 사항 반영

--- SOPT-iOS/Projects/Core/Sources/Literals/StringLiterals.swift
+++ SOPT-iOS/Projects/Core/Sources/Literals/StringLiterals.swift
@@ -263,7 +263,7 @@ public struct I18N {
     
     public struct Soptlog {
         public static let editProfile = "프로필 수정"
-        public static let enrollIntroduce = "한 줄 소개 등록하기 >"
+        public static let enrollIntroduce = "프로필 수정에서 한 줄 소개 등록해보세요!"
     }
     
     public struct Attendance {
