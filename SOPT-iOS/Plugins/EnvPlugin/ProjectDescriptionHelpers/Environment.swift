d018abc9777800fbfffff5a2bfe93de5cedcd536
yungu0010 <dbstj16@sookmyung.ac.kr>
2024-12-02 00:03:01 +0900

    [Refactor] #418 - DeploymentTargets 수정

--- SOPT-iOS/Plugins/EnvPlugin/ProjectDescriptionHelpers/Environment.swift
+++ SOPT-iOS/Plugins/EnvPlugin/ProjectDescriptionHelpers/Environment.swift
@@ -7,7 +7,7 @@ public enum Environment {
 public extension Project {
     enum Environment {
         public static let workspaceName = "SOPT-iOS"
-        public static let deploymentTarget = DeploymentTarget.iOS(targetVersion: "16.0", devices: [.iphone])
+        public static let deploymentTarget = DeploymentTargets.iOS("16.0")
         public static let platform = Platform.iOS
         public static let bundlePrefix = "com.sopt-stamp-iOS"
     }
