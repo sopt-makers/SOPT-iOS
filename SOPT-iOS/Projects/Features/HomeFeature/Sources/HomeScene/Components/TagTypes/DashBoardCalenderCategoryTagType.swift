ca8bc0fab25439200ad64eb43b418f83d2d7f6f6
yungu0010 <dbstj16@sookmyung.ac.kr>
2024-12-16 12:20:22 +0900

    [Fix] #459 - DashboardCalendarCategoryTag에 누락된 배경 색상 추가

--- SOPT-iOS/Projects/Features/HomeFeature/Sources/HomeScene/Components/TagTypes/DashBoardCalenderCategoryTagType.swift
+++ SOPT-iOS/Projects/Features/HomeFeature/Sources/HomeScene/Components/TagTypes/DashBoardCalenderCategoryTagType.swift
@@ -29,7 +29,7 @@ enum DashBoardCalenderCategoryTagType {
         case .event:
             return DSKitAsset.Colors.success.color
         case .seminar:
-            return DSKitAsset.Colors.success.color
+            return DSKitAsset.Colors.secondary.color
         }
     }
     
