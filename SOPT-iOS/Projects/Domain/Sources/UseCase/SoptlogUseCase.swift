c9bac686739b1b0355d61fa45f683f69c97dd2e4
yungu0010 <dbstj16@sookmyung.ac.kr>
2025-01-20 22:50:52 +0900

    [Feat] #477 - UseCase Layer

--- SOPT-iOS/Projects/Domain/Sources/UseCase/SoptlogUseCase.swift
+++ SOPT-iOS/Projects/Domain/Sources/UseCase/SoptlogUseCase.swift
@@ -6,4 +6,31 @@
 //  Copyright © 2025 SOPT-iOS. All rights reserved.
 //
 
-import Foundation
+import Combine
+
+import Core
+
+public protocol SoptlogUseCase {
+    func fetchSoptlogInfo() -> AnyPublisher<SoptlogModel, Never>
+}
+
+public class DefaultSoptlogUseCase: SoptlogUseCase {
+    
+    private let repository: SoptlogRepositoryInterface
+    private let cancelBag = CancelBag()
+    
+    public init(repository: SoptlogRepositoryInterface) {
+        self.repository = repository
+    }
+}
+
+extension DefaultSoptlogUseCase {
+    public func fetchSoptlogInfo() -> AnyPublisher<SoptlogModel, Never> {
+        self.repository.fetchSoptlogModel()
+            .catch { error in
+                print("Error: \(error)")
+                return Empty<SoptlogModel, Never>()
+            }
+            .eraseToAnyPublisher()
+    }
+}
