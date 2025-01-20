880b2028f4c28b7dac43ece3dd9af39550c9e18d
yungu0010 <dbstj16@sookmyung.ac.kr>
2025-01-20 22:54:36 +0900

    [Feat] #477 - SoptlogViewModel 작성 - model to presentationModel

--- SOPT-iOS/Projects/Features/SoptlogFeature/Sources/SoptlogScene/ViewModel/SoptlogViewModel.swift
+++ SOPT-iOS/Projects/Features/SoptlogFeature/Sources/SoptlogScene/ViewModel/SoptlogViewModel.swift
@@ -15,75 +15,105 @@ import Domain
 import HomeFeatureInterface
 import BaseFeatureDependency
 
-struct ProfileInfo {
-    let profileImageURL: String
-    let name: String
-    let part: String
-    let introduce: String
-}
-
-struct AppServiceInfo {
-    let serviceName: String
-    let serviceImageURL: String
-    let serviceValue: String
-}
-
-struct SoptlogAlarmInfo {
-    let imageURL: String
-    let title: String
-    let subTitle: String
-}
-
 public class SoptlogViewModel: SoptlogViewModelType {
     
-    let profileInfoList: [ProfileInfo] = [
-        ProfileInfo(
-            profileImageURL: "https://i1.sndcdn.com/artworks-000660272461-rmfvxq-t500x500.jpg",
-            name: "짱구씨",
-            part: "iOS/디자인/기획/안드로이드/웹/서버",
-            introduce: "한줄소개는 공백포함 최대15"
-        )
-    ]
+    public var onNaviBackButtonTap: (() -> Void)?
     
-    let appServiceInfoList: [AppServiceInfo] = [
-        AppServiceInfo(
-            serviceName: "솝레벨",
-            serviceImageURL: "https://i.pinimg.com/originals/99/7a/9b/997a9b2cd93277769ca9b3d109bceed7.jpg",
-            serviceValue: "Lv.6"),
-        AppServiceInfo(
-            serviceName: "콕찌르기",
-            serviceImageURL: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS5_o58idJtowURqF99s9mM1pC76aIf9t1K7w&s",
-            serviceValue: "208회"),
-        AppServiceInfo(
-            serviceName: "솝탬프",
-            serviceImageURL: "https://i.pinimg.com/736x/7d/9d/e4/7d9de429c35854f0ec32d8e0704a7d63.jpg",
-            serviceValue: "14등"),
-    ]
-    
-    let soptlogAlarmInfoList: [SoptlogAlarmInfo] = [
-        SoptlogAlarmInfo(
-            imageURL: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTcvCXwH8rPUMVwEnu5ADHwOSdwGZcqdzotiQ&s",
-            title: "짱구님, 잊지 말아야 할 말들을 듣게 될 거예요",
-            subTitle: "바로 확인하기")
-    ]
-    
+    // MARK: - Properties
+
+    private let useCase: SoptlogUseCase
+    private var cancelBag = CancelBag()
+
     // MARK: - Inputs
     
-    public struct Input { }
+    public struct Input { 
+        let viewDidLoad: Driver<Void>
+        let cellTap: Driver<IndexPath>
+    }
     
     // MARK: - Outputs
     
-    public struct Output { }
+    public struct Output {
+        let soptlogInfo = PassthroughSubject<SoptlogPresentationModel, Never>()
+    }
     
     // MARK: - initialization
     
-    public init() { }
+    public init(useCase: SoptlogUseCase, cancelBag: CancelBag = CancelBag()) {
+        self.useCase = useCase
+        self.cancelBag = cancelBag
+    }
 }
 
 extension SoptlogViewModel {
     public func transform(from input: Input, cancelBag: CancelBag) -> Output {
         let output = Output()
+        
+        input.viewDidLoad
+            .flatMap(useCase.fetchSoptlogInfo)
+            .withUnretained(self)
+            .sink { owner, soptlogModel in
+                let info = owner.transformToPresentationModel(model: soptlogModel)
+                output.soptlogInfo.send(info)
+            }.store(in: cancelBag)
+        
+        input.cellTap
+            .filter{ $0.section == 1 }
+            .withUnretained(self)
+            .sink { owner, _ in
+                // 화면 전환
+            }.store(in: cancelBag)
+        
+        input.cellTap
+            .filter{ $0.section == 2 }
+            .withUnretained(self)
+            .sink { owner, _ in
+                // 화면 전환
+            }.store(in: cancelBag)
+        
         return output
     }
 }
 
+extension SoptlogViewModel {
+    private func transformToPresentationModel(model: SoptlogModel) -> SoptlogPresentationModel {
+        var appService: [SoptlogPresentationModel.AppService] = []
+        appService.append(SoptlogPresentationModel.AppService(
+            serviceName: I18N.Soptlog.soptlevel,
+            serviceImageURL: model.icons[0],
+            serviceValue: model.soptLevel))
+        appService.append(SoptlogPresentationModel.AppService(
+            serviceName: I18N.Soptlog.poke,
+            serviceImageURL: model.icons[1],
+            serviceValue: model.pokeCount))
+        
+        if model.isActive {
+            appService.append(SoptlogPresentationModel.AppService(
+                serviceName: I18N.Soptlog.soptamp,
+                serviceImageURL: model.icons[2],
+                serviceValue: model.soptampRank))
+        } else {
+            appService.append(SoptlogPresentationModel.AppService(
+                serviceName: I18N.Soptlog.withSopt,
+                serviceImageURL: model.icons[2],
+                serviceValue: "\(model.during)개월"))
+        }
+        
+        
+        return SoptlogPresentationModel(
+            profile: SoptlogPresentationModel.Profile(
+                userName: model.userName,
+                profileImage: model.profileImage,
+                part: model.part
+            ),
+            introduce: SoptlogPresentationModel.Introduce(
+                profileMessage: model.profileMessage
+            ),
+            appService: appService,
+            alarm: SoptlogPresentationModel.Alarm(
+                isFortuneChecked: model.isFortuneChecked,
+                todayFortuneText: model.todayFortuneText
+            )
+        )
+    }
+}
