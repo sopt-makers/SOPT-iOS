4d70c86807fdc2a1a4c9bf8ce1ed6653283ba366
yungu0010 <dbstj16@sookmyung.ac.kr>
2025-01-20 22:52:34 +0900

    [Feat] #477 - PresentationModel 생성 - UseCase로부터 받은 Model을 변환하여 저장, viewModel에서는 변환된 PresentationModel을 view에 데이터스트림으로 전달하여 VC와 ViewModel의 결합도를 낮추도록 함

--- SOPT-iOS/Projects/Features/SoptlogFeature/Sources/SoptlogScene/Model/SoptlogPresentationModel.swift
+++ SOPT-iOS/Projects/Features/SoptlogFeature/Sources/SoptlogScene/Model/SoptlogPresentationModel.swift
@@ -7,3 +7,31 @@
 //
 
 import Foundation
+
+struct SoptlogPresentationModel {
+    let profile: Profile
+    let introduce: Introduce
+    let appService: [AppService]
+    let alarm: Alarm
+    
+    struct Profile {
+        let userName: String
+        let profileImage: String
+        let part: String
+    }
+    
+    struct Introduce {
+        let profileMessage: String
+    }
+
+    struct AppService {
+        let serviceName: String
+        let serviceImageURL: String
+        let serviceValue: String
+    }
+
+    struct Alarm {
+        let isFortuneChecked: Bool
+        let todayFortuneText: String
+    }
+}
