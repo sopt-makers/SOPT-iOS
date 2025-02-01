dc9e9396d5020806ca52f52b32ccc992bcea74ea
yungu0010 <dbstj16@sookmyung.ac.kr>
2025-02-02 02:05:54 +0900

    [Setting] #491 - 템플릿에 필요한 plist, stencil 파일 생성

--- SOPT-iOS/Tuist/Templates/feature/feature.swift
+++ SOPT-iOS/Tuist/Templates/feature/feature.swift
@@ -13,5 +13,41 @@ let template = Template(
     description: "Creates a new feature module",
     attributes: [
         nameAttribute
+    ],
+    items: [
+        // Main
+        .directory(path: "Projects/Features/\(nameAttribute)Feature", sourcePath: ""),
+        .file(path: "Projects/Features/\(nameAttribute)Feature/Project.swift", templatePath: "Project.stencil"),
+        
+        // Tests
+        .directory(path: "Projects/Features/\(nameAttribute)Feature/Tests", sourcePath: "./"),
+        .directory(path: "Projects/Features/\(nameAttribute)Feature/Tests/Sources", sourcePath: "./"),
+        .file(path: "Projects/Features/\(nameAttribute)Feature/Tests/Sources/.empty", templatePath: ".empty"),
+        .directory(path: "Projects/Features/\(nameAttribute)Feature/Tests/Resources", sourcePath: "./"),
+        .file(path: "Projects/Features/\(nameAttribute)Feature/Tests/Resources/.empty", templatePath: ".empty"),
+        
+        // Sources
+        .directory(path: "Projects/Features/\(nameAttribute)Feature/Sources", sourcePath: "./"),
+        .file(path: "Projects/Features/\(nameAttribute)Feature/Sources/.empty", templatePath: ".empty"),
+        
+        // Interface
+        .directory(path: "Projects/Features/\(nameAttribute)Feature/Interface", sourcePath: "./"),
+        .directory(path: "Projects/Features/\(nameAttribute)Feature/Interface/Sources", sourcePath: "./"),
+        .file(path: "Projects/Features/\(nameAttribute)Feature/Interface/Sources/.empty", templatePath: ".empty"),
+        
+        // Derived
+        .directory(path: "Projects/Features/\(nameAttribute)Feature/Derived", sourcePath: "./"),
+        .directory(path: "Projects/Features/\(nameAttribute)Feature/Derived/Sources", sourcePath: "./"),
+        .file(path: "Projects/Features/\(nameAttribute)Feature/Derived/Sources/.empty", templatePath: ".empty"),
+        .directory(path: "Projects/Features/\(nameAttribute)Feature/Derived/InfoPlists", sourcePath: "./"),
+        .file(path: "Projects/Features/\(nameAttribute)Feature/Derived/InfoPlists/Info.plist", templatePath: "Info.plist"),
+        
+        // Demo
+        .directory(path: "Projects/Features/\(nameAttribute)Feature/Demo", sourcePath: "./"),
+        .directory(path: "Projects/Features/\(nameAttribute)Feature/Demo/Resources", sourcePath: "./"),
+        .file(path: "Projects/Features/\(nameAttribute)Feature/Demo/Sources/LaunchScreen.storyboard", templatePath: "LaunchScreen.storyboard"),
+        .directory(path: "Projects/Features/\(nameAttribute)Feature/Demo/Sources/Assets.xcassets", sourcePath: "Assets.xcassets"),
+        .directory(path: "Projects/Features/\(nameAttribute)Feature/Demo/Sources", sourcePath: "./"),
+        .file(path: "Projects/Features/\(nameAttribute)Feature/Demo/Sources/.empty", templatePath: ".empty"),
     ]
 )
