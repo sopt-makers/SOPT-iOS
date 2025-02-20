88a504d3028742b36c016ba84653cd0a982afde6
yungu0010 <dbstj16@sookmyung.ac.kr>
2025-02-20 13:07:18 +0900

    [Feat] #460 - 탭 바 UI 구현

---
+++ SOPT-iOS/Projects/Features/TabBarFeature/Sources/TabBarController.swift
@@ -0,0 +1,68 @@
+//
+//  TabBarItem.swift
+//  TabBarFeature
+//
+//  Created by 강윤서 on 2/20/25.
+//  Copyright © 2025 SOPT-iOS. All rights reserved.
+//
+
+import UIKit
+
+import DSKit
+
+final class TabBarController: UITabBarController {
+    
+    private let tabList: [UINavigationController]
+    
+    init(tabList: [UINavigationController]) {
+        self.tabList = tabList
+        
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        configureTabBar()
+        configureTabBarItem()
+    }
+    
+    override public func viewDidLayoutSubviews() {
+        configureTabBarHeight()
+    }
+}
+
+extension TabBarController {
+    private func configureTabBar() {
+        UITabBar.clearShadow()
+        view.tintColor = .white
+        tabBar.backgroundColor = DSKitAsset.Colors.gray800.color
+        
+        tabBar.layer.cornerRadius = 20
+        tabBar.layer.masksToBounds = true
+        tabBar.layer.maskedCorners = [.layerMinXMinYCorner, .layerMaxXMinYCorner]
+    }
+    
+    /// setting tabbar height
+    private func configureTabBarHeight() {
+        var tabFrame = tabBar.frame
+        tabFrame.size.height = 72
+        tabFrame.origin.y = view.frame.size.height - tabFrame.size.height
+        
+        tabBar.frame = tabFrame
+    }
+    
+    private func configureTabBarItem() {
+        TabBarItem.allCases.forEach {
+            tabList[$0.rawValue].tabBarItem = $0.makeTabBarItem()
+            tabList[$0.rawValue].tabBarItem.tag = $0.rawValue
+            tabList[$0.rawValue].tabBarItem.imageInsets = UIEdgeInsets(top: 18, left: 0, bottom: -18, right: 0)
+        }
+
+        setViewControllers(tabList, animated: true)
+    }
+}
