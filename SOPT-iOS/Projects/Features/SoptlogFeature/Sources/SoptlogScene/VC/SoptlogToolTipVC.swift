941e18bb48f891b471cd3ff41eed277f8a0d5e42
yungu0010 <dbstj16@sookmyung.ac.kr>
2025-03-26 01:10:59 +0900

    [Add] #521 - 툴팁 이미지 추가

---
+++ SOPT-iOS/Projects/Features/SoptlogFeature/Sources/SoptlogScene/VC/SoptlogToolTipVC.swift
@@ -0,0 +1,22 @@
+//
+//  SoptlogToopTipVC.swift
+//  SoptlogFeature
+//
+//  Created by 강윤서 on 3/26/25.
+//  Copyright © 2025 SOPT-iOS. All rights reserved.
+//
+
+import UIKit
+
+final class SoptlogToopTipVC: UIViewController {
+
+    // MARK: - UI Components
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
