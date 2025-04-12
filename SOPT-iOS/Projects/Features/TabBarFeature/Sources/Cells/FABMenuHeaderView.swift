d5ef9863066fb658e63114a011b354a4f2d0eeea
yungu0010 <dbstj16@sookmyung.ac.kr>
2025-04-13 00:52:21 +0900

    [Feat] #539 - FAB section Header View 구현

--- SOPT-iOS/Projects/Features/TabBarFeature/Sources/Cells/FABMenuHeaderView.swift
+++ SOPT-iOS/Projects/Features/TabBarFeature/Sources/Cells/FABMenuHeaderView.swift
@@ -8,6 +8,37 @@
 
 import UIKit
 
-class FABMenuHeaderView: UICollectionReusableView {
+import DSKit
+
+final class FABMenuHeaderView: UICollectionReusableView {
+      
+    // MARK: - UI Components
+    
+    private let sectionTitle = UILabel().then{
+        $0.font = DSKitFontFamily.Suit.medium.font(size: 12)
+        $0.textColor = DSKitAsset.Colors.gray950.color
+    }
+    
+    // MARK: - Life Cycle
+    override init(frame: CGRect) {
+        super.init(frame: frame)
         
+        setLayout()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+// MARK: - UI & Layout
+
+extension FABMenuHeaderView {
+    private func setLayout() {
+        addSubviews(sectionTitle)
+        
+        sectionTitle.snp.makeConstraints { make in
+            make.leading.equalToSuperview()
+        }
+    }
 }
