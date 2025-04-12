518ed30910bbbfba795916e854e987873d1e8b26
yungu0010 <dbstj16@sookmyung.ac.kr>
2025-04-13 00:27:30 +0900

    [Feat] #539 - FAB Cell 구현

---
+++ SOPT-iOS/Projects/Features/TabBarFeature/Sources/Cells/FABMenuCVC.swift
@@ -0,0 +1,62 @@
+//
+//  FABMenuCVC.swift
+//  TabBarFeature
+//
+//  Created by 강윤서 on 4/13/25.
+//  Copyright © 2025 SOPT-iOS. All rights reserved.
+//
+
+import UIKit
+
+import DSKit
+
+final class FABMenuCVC: UICollectionViewCell {
+    
+    // MARK: - UI Componets
+    
+    private let menuImage = UIImageView()
+    private let menuTitle = UILabel().then {
+        $0.font = DSKitFontFamily.Suit.medium.font(size: 14)
+        $0.textColor = DSKitAsset.Colors.gray950.color
+    }
+    
+    // MARK: - Life Cycle
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        setLayout()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+// MARK: - UI & Layout
+
+extension FABMenuCVC {
+    private func setLayout() {
+        contentView.addSubviews(menuImage, menuTitle)
+        
+        menuImage.snp.makeConstraints{
+            $0.size.equalTo(22)
+            $0.leading.centerY.equalToSuperview()
+        }
+        
+        menuTitle.snp.makeConstraints{
+            $0.leading.equalTo(menuImage.snp.trailing).offset(6)
+        }
+    }
+}
+
+// MARK: - Methods
+
+extension FABMenuCVC {
+    private func configureCell() {
+        
+        //TODO: - 데이터 전달 시 수정
+        menuImage.image = .actions
+        menuTitle.text = "메뉴"
+    }
+}
---
+++ SOPT-iOS/Projects/Features/TabBarFeature/Sources/Controller/TabBarFloatingButtonVC.swift
@@ -0,0 +1,30 @@
+//
+//  TabBarFloatingButtonVC.swift
+//  TabBarFeature
+//
+//  Created by 강윤서 on 4/13/25.
+//  Copyright © 2025 SOPT-iOS. All rights reserved.
+//
+
+import UIKit
+
+class TabBarFloatingButtonVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
