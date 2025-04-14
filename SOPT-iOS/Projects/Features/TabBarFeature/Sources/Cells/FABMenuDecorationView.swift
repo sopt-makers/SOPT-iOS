645ff0a4f93889b460748b9450ca0044eff660bd
yungu0010 <dbstj16@sookmyung.ac.kr>
2025-04-14 20:00:19 +0900

    [Feat] #539 - collectionView Section DecorationView 구현

--- SOPT-iOS/Projects/Features/TabBarFeature/Sources/Cells/FABMenuDecorationView.swift
+++ SOPT-iOS/Projects/Features/TabBarFeature/Sources/Cells/FABMenuDecorationView.swift
@@ -8,6 +8,36 @@
 
 import UIKit
 
-class FABMenuDecorationView: UICollectionReusableView {
+import DSKit
+
+final class FABMenuDecorationView: UICollectionReusableView {
+    
+    // MARK: - UI Components
+    private let backgroundView = UIView().then {
+        $0.backgroundColor = .white
+        $0.layer.cornerRadius = 16
+    }
         
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        setLayout()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+// MARK: - UI & Layout
+
+extension FABMenuDecorationView {
+    private func setLayout() {
+        self.addSubviews(backgroundView)
+        
+        backgroundView.snp.makeConstraints { make in
+            make.top.leading.trailing.equalToSuperview()
+            make.bottom.equalToSuperview().inset(8)
+        }
+    }
 }
