1df41adc3a86a1b266f7c3493fe2f5d44f076b64
yungu0010 <dbstj16@sookmyung.ac.kr>
2025-04-14 20:01:22 +0900

    [Feat] #539 - 섹션과 섹션 아이템 구성

--- SOPT-iOS/Projects/Features/TabBarFeature/Sources/CollectionView/TabBarMenuSection.swift
+++ SOPT-iOS/Projects/Features/TabBarFeature/Sources/CollectionView/TabBarMenuSection.swift
@@ -7,3 +7,57 @@
 //
 
 import Foundation
+import UIKit
+
+import Core
+import DSKit
+
+
+struct MenuSectionItem {
+    var title: String
+    var icon: UIImage
+}
+
+protocol TabBarMenuSectionProtocol {
+    var title: String { get }
+    var items: [MenuSectionItem] { get }
+}
+
+enum TabBarMenuSection: CaseIterable, TabBarMenuSectionProtocol {
+    case playground
+    case groupAndStudy
+    case homepage
+    
+    var title: String {
+        switch self {
+        case .playground:
+            return I18N.TabBar.playground
+        case .groupAndStudy:
+            return I18N.TabBar.groupAndStudy
+        case .homepage:
+            return I18N.TabBar.homepage
+        }
+    }
+    
+    var items: [MenuSectionItem] {
+        switch self {
+        case .playground:
+            return [MenuSectionItem(title: I18N.TabBar.Playground.write, 
+                                    icon: DSKitAsset.Assets.icFabPencil.image)]
+        case .groupAndStudy:
+            return [
+                MenuSectionItem(title: I18N.TabBar.GroupAndStudy.makeGroup, 
+                                icon: DSKitAsset.Assets.icFabGroup.image),
+                MenuSectionItem(title: I18N.TabBar.GroupAndStudy.makeLightGroup,
+                                icon: DSKitAsset.Assets.icFabBolt.image),
+                MenuSectionItem(title: I18N.TabBar.GroupAndStudy.writeFeed,
+                                icon: DSKitAsset.Assets.icFabFire.image)
+            ]
+        case .homepage:
+            return [
+                MenuSectionItem(title: I18N.TabBar.Homepage.uploadArticle, 
+                                icon: DSKitAsset.Assets.icFabHomepage.image),
+            ]
+        }
+    }
+}
