1b57be14a865b63876851feceec37a2ccade575c
yungu0010 <dbstj16@sookmyung.ac.kr>
2025-04-26 02:55:08 +0900

    [Merge] #418 - conflict 해결

--- SOPT-iOS/Plugins/EnvPlugin/ProjectDescriptionHelpers/Environment.swift
+++ SOPT-iOS/Plugins/EnvPlugin/ProjectDescriptionHelpers/Environment.swift
@@ -8,7 +8,6 @@ public extension Project {
     enum Environment {
         public static let workspaceName = "SOPT-iOS"
         public static let deploymentTarget = DeploymentTargets.iOS("16.0")
-        public static let platform = Platform.iOS
         public static let bundlePrefix = "com.sopt-stamp-iOS"
     }
 }
