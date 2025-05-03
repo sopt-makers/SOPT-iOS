64fc44d27987d3404fc25242fc1bdcbf0e421756
dlwogus0128 <79050615+dlwogus0128@users.noreply.github.com>
2025-05-03 19:18:48 +0900

    [Refactor] #516 - Router 제거한 SplashCoor 생성

---
+++ SOPT-iOS/Projects/Features/SplashFeature/Sources/SplashScene/Coordinator/SplashCoordinator.swift
@@ -0,0 +1,77 @@
+//
+//  SplashCoordinator.swift
+//  SplashFeature
+//
+//  Created by Jae Hyun Lee on 5/3/25.
+//  Copyright © 2025 SOPT-iOS. All rights reserved.
+//
+
+import UIKit
+
+import BaseFeatureDependency
+import SplashFeatureInterface
+import Core
+import Domain
+
+public final class SplashCoordinator: DefaultCoordinator {
+    
+    // MARK: - Properties
+    
+    public var finishFlow: (() -> Void)?
+    
+    private let navigationController: UINavigationController
+    private let factory: LegacySplashFeatureViewBuildable
+    private let cancelBag = CancelBag()
+    
+    // MARK: - Init
+    
+    public init(
+        navigationController: UINavigationController,
+        factory: LegacySplashFeatureViewBuildable
+    ) {
+        self.factory = factory
+        self.navigationController = navigationController
+        super.init()
+    }
+    
+    // MARK: - Coordinator Life Cycle
+    
+    public override func start() {
+        showSplash()
+    }
+    
+    // MARK: - Navigation
+    
+    private func showSplash() {
+        var splash = factory.makeSplash()
+        splash.vm.onNoticeExist = { [weak self] appNoticeModel in
+            self?.presentNoticePopUp(model: appNoticeModel)
+        }
+        splash.vm.onNoticeSkipped = { [weak self] in
+            self?.finishFlow?()
+        }
+        
+        navigationController.setViewControllers([splash.vc.viewController], animated: true)
+    }
+    
+    private func presentNoticePopUp(model: AppNoticeModel) {
+        guard let isForcedUpdate = model.isForced else { return }
+        
+        let popUpType: NoticePopUpType = isForcedUpdate ? .forceUpdate : .recommendUpdate
+        
+        let noticePopUp = factory.makeNoticePopUpVC(
+            noticeType: popUpType,
+            content: model.notice
+        )
+        
+        noticePopUp.closeButtonTappedWithCheck.sink { [weak self] didCheck in
+            if didCheck {
+                UserDefaultKeyList.AppNotice.checkedAppVersion = model.recommendVersion
+            }
+            self?.navigationController.dismiss(animated: true)
+            self?.finishFlow?()
+        }.store(in: cancelBag)
+        
+        navigationController.present(noticePopUp.viewController, animated: false)
+    }
+}
