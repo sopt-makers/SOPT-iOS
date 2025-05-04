fd5d2d0a544a40dd025e151a83afab17cec81e59
dlwogus0128 <79050615+dlwogus0128@users.noreply.github.com>
2025-05-05 01:16:33 +0900

    [Refactor] #560 - CoordinatorUtils 구현
    Router: 리팩 이후 화면 전환에 필요한 함수들 이전

---
+++ SOPT-iOS/Projects/Features/BaseFeatureDependency/Sources/Coordinator/CoordinatorUtils.swift
@@ -0,0 +1,39 @@
+//
+//  CoordinatorUtils.swift
+//  BaseFeatureDependency
+//
+//  Created by Jae Hyun Lee on 5/5/25.
+//  Copyright © 2025 SOPT-iOS. All rights reserved.
+//
+
+import UIKit
+
+public enum CoordinatorUtils {
+    /// 윈도우의 rootViewController를, 주입받은 viewController를 감싼 navigationController로 교체합니다.
+    public static func replaceAsRootWindow(
+        root navigation: UINavigationController,
+        animated: Bool = true,
+        hideBar: Bool = false
+    ) {
+        guard let window = UIWindow.keyWindowGetter else { return }
+        navigation.isNavigationBarHidden = hideBar
+
+        if !animated {
+            window.rootViewController = navigation
+            window.makeKeyAndVisible()
+            return
+        }
+
+        if let snapshot = window.snapshotView(afterScreenUpdates: true) {
+            navigation.view.addSubview(snapshot)
+            window.rootViewController = navigation
+            window.makeKeyAndVisible()
+
+            UIView.animate(withDuration: 0.4, animations: {
+                snapshot.layer.opacity = 0
+            }, completion: { _ in
+                snapshot.removeFromSuperview()
+            })
+        }
+    }
+}
