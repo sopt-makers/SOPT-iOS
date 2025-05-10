bfe777dcf3490e69ff982920b5cb40f135fab85c
yungu0010 <dbstj16@sookmyung.ac.kr>
2025-05-10 15:20:49 +0900

    [Add] #561 - color 에셋 추가

---
+++ SOPT-iOS/Projects/Features/HomeFeature/Sources/HomeScene/Components/ExtendedFAButton.swift
@@ -0,0 +1,21 @@
+//
+//  ExtendedFAButton.swift
+//  HomeFeature
+//
+//  Created by 강윤서 on 5/10/25.
+//  Copyright © 2025 SOPT-iOS. All rights reserved.
+//
+
+import UIKit
+
+class ExtendedFAButton: UIButton {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
