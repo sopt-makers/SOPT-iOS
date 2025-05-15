86c864f52b8421942a21aa422b25ba1323e17cef
yungu0010 <dbstj16@sookmyung.ac.kr>
2025-05-15 17:18:09 +0900

    [Fix] #561 - pokeCoordinator가 종료될 때 자식 코디네이터의 의존성을 제거

--- SOPT-iOS/Projects/Features/RootFeature/Sources/ApplicationCoordinator.swift
+++ SOPT-iOS/Projects/Features/RootFeature/Sources/ApplicationCoordinator.swift
@@ -369,6 +369,11 @@ extension ApplicationCoordinator {
         addDependency(coordinator)
         coordinator.start()
         
+        coordinator.finishFlow = { [weak self, weak coordinator] in
+            coordinator?.childCoordinators = []
+            self?.removeDependency(coordinator)
+        }
+        
         return coordinator
     }
     
