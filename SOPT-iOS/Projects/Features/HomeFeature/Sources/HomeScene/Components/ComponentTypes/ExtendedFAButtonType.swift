e6c099b5a4a51ec1e7ed776c12277d99c0c98690
yungu0010 <dbstj16@sookmyung.ac.kr>
2025-05-15 15:45:24 +0900

    [Feat] #561 - ExtendedFAButton Type 분리

---
+++ SOPT-iOS/Projects/Features/HomeFeature/Sources/HomeScene/Components/ComponentTypes/ExtendedFAButtonType.swift
@@ -0,0 +1,12 @@
+//
+//  ExtendedFAButtonType.swift
+//  HomeFeature
+//
+//  Created by 강윤서 on 5/15/25.
+//  Copyright © 2025 SOPT-iOS. All rights reserved.
+//
+
+enum ExtendedFAButtonType {
+    case extended
+    case collapsed
+}
