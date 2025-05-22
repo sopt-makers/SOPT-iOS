51535b292d135905f40d16d1893164dfdc672fd8
yungu0010 <dbstj16@sookmyung.ac.kr>
2025-05-22 18:11:14 +0900

    [Del] #567 - 불필요한 주석, 생성자 삭제

--- SOPT-iOS/Projects/Modules/Networks/Sources/Entity/ResponseEntity/SoptlogResponseEntity.swift
+++ SOPT-iOS/Projects/Modules/Networks/Sources/Entity/ResponseEntity/SoptlogResponseEntity.swift
@@ -8,7 +8,6 @@
 
 import Foundation
 
-// MARK: - 한 줄 소개와 솝마디 추가되어야 함. 파트 방식 변경 요청 상태.
 public struct SoptlogResponseEntity: Decodable {
     public let userName: String?
     public let profileImage: String?
@@ -22,19 +21,4 @@ public struct SoptlogResponseEntity: Decodable {
     public let isFortuneChecked: Bool
     public let todayFortuneText: String
     public let isActive: Bool
-    
-    public init(userName: String?, profileImage: String?, part: String, soptampRank: String?, pokeCount: String, soptLevel: String, during: String, profileMessage: String, icons: [String], isFortuneChecked: Bool, todayFortuneText: String, isActive: Bool) {
-        self.userName = userName
-        self.profileImage = profileImage
-        self.part = part
-        self.soptampRank = soptampRank
-        self.pokeCount = pokeCount
-        self.soptLevel = soptLevel
-        self.during = during
-        self.profileMessage = profileMessage
-        self.icons = icons
-        self.isFortuneChecked = isFortuneChecked
-        self.todayFortuneText = todayFortuneText
-        self.isActive = isActive
-    }
 }
