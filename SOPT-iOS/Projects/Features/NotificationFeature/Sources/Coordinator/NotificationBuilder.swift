88c819856194b0912e6d523c59643a4d6236e5c2
dlwogus0128 <79050615+dlwogus0128@users.noreply.github.com>
2025-05-27 01:04:00 +0900

    [Refactor] #563 - 리팩토링 Builder 생성

---
+++ SOPT-iOS/Projects/Features/NotificationFeature/Sources/Coordinator/NotificationBuilder.swift
@@ -0,0 +1,36 @@
+//
+//  NotificationBuilder.swift
+//  NotificationFeatureInterface
+//
+//  Created by Jae Hyun Lee on 5/27/25.
+//  Copyright © 2025 SOPT-iOS. All rights reserved.
+//
+
+import Core
+import Domain
+@_exported import NotificationFeatureInterface
+
+public
+final class NotificationBuilder {
+    @Injected public var notificationListRepository: NotificationListRepositoryInterface
+    @Injected public var notificationDetailRepository: NotificationDetailRepositoryInterface
+    
+    public init() { }
+}
+
+extension NotificationBuilder: NotificationFeatureBuildable {
+    public func makeNotificationList() -> NotificationListPresentable {
+        let useCase = DefaultNotificationListUseCase(repository: notificationListRepository)
+        let vm = NotificationListViewModel(useCase: useCase)
+        let vc = NotificationListVC(viewModel: vm)
+        return (vc, vm)
+    }
+    
+    public func makeNotificationDetailVC(notificationId: String) -> NotificationDetailPresentable {
+        let useCase = DefaultNotificationDetailUseCase(repository: notificationDetailRepository)
+        let viewModel = NotificationDetailViewModel(useCase: useCase, notificationId: notificationId)
+        let notificationDetailVC = NotificationDetailVC(viewModel: viewModel)
+        
+        return (notificationDetailVC, viewModel)
+    }
+}
