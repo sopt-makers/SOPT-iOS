ce839e2b4c7ddf10c34ea3b60418ca2ca54d67d3
dlwogus0128 <79050615+dlwogus0128@users.noreply.github.com>
2025-05-27 01:37:43 +0900

    [Refactor] #563 - Router 의존성 제거한 Coor 생성

---
+++ SOPT-iOS/Projects/Features/NotificationFeature/Sources/Coordinator/NotificationCoordinator.swift
@@ -0,0 +1,76 @@
+//
+//  NotificationCoordinator.swift
+//  NotificationFeatureInterface
+//
+//  Created by Jae Hyun Lee on 5/27/25.
+//  Copyright © 2025 SOPT-iOS. All rights reserved.
+//
+
+import UIKit
+
+import BaseFeatureDependency
+import NotificationFeatureInterface
+import Domain
+import Core
+
+public final class NotificationCoordinator: DefaultNotificationCoordinator {
+    
+    // MARK: - Properties
+    
+    public var requestCoordinating: ((NotificationCoordinatorDestination) -> Void)?
+    public var finishFlow: (() -> Void)?
+
+    private let navigationController: UINavigationController
+    private let factory: NotificationFeatureBuildable
+    
+    // MARK: - Init
+    
+    public init(
+        navigationController: UINavigationController,
+        factory: NotificationFeatureBuildable
+    ) {
+        self.navigationController = navigationController
+        self.factory = factory
+        super.init()
+    }
+    
+    // MARK: - Coordinator Life Cycle
+
+    public override func start() {
+        showNotificationList()
+    }
+
+    // MARK: - Navigation
+    
+    private func showNotificationList() {
+        var notificationList = factory.makeNotificationList()
+        
+        notificationList.vm.onNaviBackButtonTap = { [weak self] in
+            self?.navigationController.popViewController(animated: true)
+            self?.finishFlow?()
+        }
+        
+        notificationList.vm.onNotificationTap = { [weak self] notificationId in
+            self?.showNotificationDetail(notificationId: notificationId)
+        }
+        
+        navigationController.pushViewController(notificationList.vc, animated: true)
+    }
+
+    public func showNotificationDetail(notificationId: String) {
+        var notificationDetail = factory.makeNotificationDetailVC(notificationId: notificationId)
+        
+        notificationDetail.vm.onShortCutButtonTap = { [weak self] link in
+            let url = link.url
+            let destination: NotificationCoordinatorDestination = link.isDeepLink ? .deepLink(url: url) : .webLink(url: url)
+            AmplitudeInstance.shared.track(eventType: .viewNotificationDetail, eventProperties: [
+                "notification_id": notificationId,
+                "open_method": link.isDeepLink ? "푸시알림" : "알림센터",
+                "contain_deeplink": link.isDeepLink
+            ])
+            self?.requestCoordinating?(destination)
+        }
+        
+        navigationController.pushViewController(notificationDetail.vc, animated: true)
+    }
+}
