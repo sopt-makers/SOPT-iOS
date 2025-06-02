1a1c5b4ce77a9174d0ba5bdbcdf5eb6ecb7260c8
yungu0010 <dbstj16@sookmyung.ac.kr>
2025-06-03 03:04:24 +0900

    [Feat] #601 - MyPageSection enum 생성

---
+++ SOPT-iOS/Projects/Features/AppMyPageFeature/Sources/AppMypageScene/CollectionView/SectionLayoutKind/MyPageSectionLayoutKind.swift
@@ -0,0 +1,50 @@
+//
+//  MyPageSectionLayoutKind.swift
+//  AppMyPageFeature
+//
+//  Created by 강윤서 on 6/2/25.
+//  Copyright © 2025 SOPT-iOS. All rights reserved.
+//
+
+import Core
+
+enum MyPageSectionLayoutKind: Int, CaseIterable {
+    case servicePolicy
+    case notificationSettings
+    case soptampSettings
+    case etc
+    
+    var title: String {
+        switch self {
+        case .servicePolicy: return I18N.MyPage.ServicePolicySection.title
+        case .notificationSettings: return I18N.MyPage.NotificationSection.title
+        case .soptampSettings: return I18N.MyPage.SoptampSection.title
+        case .etc: return I18N.MyPage.EtcSection.title
+        }
+    }
+    
+    var items: [MyPageItem] {
+        switch self {
+        case .servicePolicy:
+            return [
+                MyPageItem(title: I18N.MyPage.ServicePolicySection.privacyPolicy),
+                MyPageItem(title: I18N.MyPage.ServicePolicySection.termsOfUse),
+                MyPageItem(title: I18N.MyPage.ServicePolicySection.sendFeedback)
+            ]
+        case .notificationSettings:
+            return [
+                MyPageItem(title: I18N.MyPage.NotificationSection.setNotification)
+            ]
+        case .soptampSettings:
+            return [
+                MyPageItem(title: I18N.MyPage.SoptampSection.editOnelineSentence),
+                MyPageItem(title: I18N.MyPage.SoptampSection.resetStamp)
+            ]
+        case .etc:
+            return [
+                MyPageItem(title: I18N.MyPage.EtcSection.logout),
+                MyPageItem(title: I18N.MyPage.EtcSection.withdrawal),
+            ]
+        }
+    }
+}
