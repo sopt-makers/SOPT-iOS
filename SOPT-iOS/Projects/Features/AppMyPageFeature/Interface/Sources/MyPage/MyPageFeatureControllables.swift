a2ecd4aacdc8fe9d74c3df79b870520563c21440
yungu0010 <dbstj16@sookmyung.ac.kr>
2025-06-04 03:01:27 +0900

    [Refactor] #564 - WithdrawalPresentable 생성(UIViewController, ViewModelType)

---
+++ SOPT-iOS/Projects/Features/AppMyPageFeature/Interface/Sources/MyPage/MyPageFeatureControllables.swift
@@ -0,0 +1,17 @@
+//
+//  MyPageFeatureControllables.swift
+//  AppMyPageFeature
+//
+//  Created by 강윤서 on 6/4/25.
+//  Copyright © 2025 SOPT-iOS. All rights reserved.
+//
+
+import UIKit
+import Core
+
+public protocol WithdrawalViewCoordinatable {
+    var onWithdrawal: (() -> Void)? { get set }
+}
+
+public typealias WithdrawalViewModelType = ViewModelType & WithdrawalViewCoordinatable
+public typealias WithdrawalPresentable = (vc: UIViewController, vm: any WithdrawalViewModelType)
