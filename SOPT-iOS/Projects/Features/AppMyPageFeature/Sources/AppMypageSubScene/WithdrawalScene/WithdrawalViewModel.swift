d7697cf32d89040821be701249fc1369dd199267
yungu0010 <dbstj16@sookmyung.ac.kr>
2025-06-04 03:06:08 +0900

    [Fix] #564 - WithdrawalVM이 Coordiantable을 준수하도록 함

--- SOPT-iOS/Projects/Features/AppMyPageFeature/Sources/AppMypageSubScene/WithdrawalScene/WithdrawalViewModel.swift
+++ SOPT-iOS/Projects/Features/AppMyPageFeature/Sources/AppMypageSubScene/WithdrawalScene/WithdrawalViewModel.swift
@@ -11,8 +11,9 @@ import Combine
 import Core
 import Domain
 
-public class WithdrawalViewModel: ViewModelType {
-
+public class WithdrawalViewModel: ViewModelType, WithdrawalViewCoordinatable {
+    public var onWithdrawal: (() -> Void)?
+    
     private let useCase: SettingUseCase
     private var cancelBag = CancelBag()
     
