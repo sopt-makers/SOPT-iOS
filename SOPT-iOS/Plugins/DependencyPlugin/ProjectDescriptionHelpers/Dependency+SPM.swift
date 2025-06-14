3c60ce2096a59e066c627b5def6927244aa963bc
dlwogus0128 <79050615+dlwogus0128@users.noreply.github.com>
2025-06-14 23:43:58 +0900

    [Setting] #421 - FirebaseCrashlytics 의존성 추가

--- SOPT-iOS/Plugins/DependencyPlugin/ProjectDescriptionHelpers/Dependency+SPM.swift
+++ SOPT-iOS/Plugins/DependencyPlugin/ProjectDescriptionHelpers/Dependency+SPM.swift
@@ -25,4 +25,6 @@ public extension TargetDependency.SPM {
     static let Quick = TargetDependency.external(name: "Quick")
     static let lottie = TargetDependency.external(name: "Lottie")
     static let Amplitude = TargetDependency.external(name: "AmplitudeSwift")
+    static let FirebaseCrashlytics = TargetDependency.external(name: "FirebaseCrashlytics")
+    static let FirebaseCore = TargetDependency.external(name: "FirebaseCore")
 }
--- SOPT-iOS/Tuist/Package.swift
+++ SOPT-iOS/Tuist/Package.swift
@@ -19,6 +19,7 @@ let packageSettings = PackageSettings(
         "Nimble": .staticFramework,
         "Lottie": .staticFramework,
         "AmplitudeSwift": .staticFramework,
+        "Firebase": .framework
     ],
     baseSettings: .settings(
         configurations: [
@@ -48,5 +49,6 @@ let package = Package(
         .package(url: "https://github.com/Quick/Nimble.git", from: "12.0.0"),
         .package(url: "https://github.com/airbnb/lottie-ios", from: "4.5.0"),
         .package(url: "https://github.com/amplitude/Amplitude-Swift", from: "1.11.10"),
+        .package(url: "https://github.com/firebase/firebase-ios-sdk.git", from: "11.14.0")
     ]
 )
