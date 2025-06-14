fb406ebb2d07abae082f2c87328a4c91ab4688a1
dlwogus0128 <79050615+dlwogus0128@users.noreply.github.com>
2025-06-14 23:45:40 +0900

    [Add] #421 - 필요 Scripts 추가
    target에 따라 info 파일 경로 변경, dSYM 업로드 스크립트

--- SOPT-iOS/Tuist/ProjectDescriptionHelpers/Scripts.swift
+++ SOPT-iOS/Tuist/ProjectDescriptionHelpers/Scripts.swift
@@ -21,4 +21,35 @@ else
     echo "warning: SwiftLint not installed, download from https://github.com/realm/SwiftLint"
 fi
 """, name: "SwiftLintString", basedOnDependencyAnalysis: false)
+    
+    static let googleServiceInfo = TargetScript.pre(
+    script: """
+    case "${TARGET_NAME}" in
+    "SOPT-iOS" )
+      cp -r "$SRCROOT/Resources/GoogleService-Info.plist" \\
+            "${BUILT_PRODUCTS_DIR}/${PRODUCT_NAME}.app/GoogleService-Info.plist"
+      ;;
+    "SOPT-iOS-Demo" )
+      cp -r "$SRCROOT/../Demo/Resources/GoogleService-Info.plist" \\
+            "${BUILT_PRODUCTS_DIR}/${PRODUCT_NAME}.app/GoogleService-Info.plist"
+      ;;
+    esac
+    """,
+    name: "Copy GoogleService-Info.plist"
+    )
+    
+    static let uploadDSYMToFirebaseScript = TargetScript.post(
+      script: """
+        "$SRCROOT/../../Tuist/Dependencies/SwiftPackageManager/.build/checkouts/firebase-ios-sdk/Crashlytics/run"
+      """,
+      name: "Upload dSYM to Firebase",
+      inputPaths: [
+        "${DWARF_DSYM_FOLDER_PATH}/${DWARF_DSYM_FILE_NAME}",
+        "${DWARF_DSYM_FOLDER_PATH}/${DWARF_DSYM_FILE_NAME}/Contents/Resources/DWARF/${PRODUCT_NAME}",
+        "${DWARF_DSYM_FOLDER_PATH}/${DWARF_DSYM_FILE_NAME}/Contents/Info.plist",
+        "$(TARGET_BUILD_DIR)/$(UNLOCALIZED_RESOURCES_FOLDER_PATH)/GoogleService-Info.plist",
+        "$(TARGET_BUILD_DIR)/$(EXECUTABLE_PATH)",
+        "${DWARF_DSYM_FOLDER_PATH}/${DWARF_DSYM_FILE_NAME}/Contents/Resources/DWARF/${PRODUCT_NAME}.debug.dylib"
+      ]
+    )
 }
