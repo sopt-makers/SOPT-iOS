dc3c7d004e95e34cde566c34a273d9b29d58083f
Melt <seokwoo2000@naver.com>
2025-06-21 21:12:14 +0900

    [Feat] #645 - refresh를 하지 않는 authinterceptor 구현

---
+++ SOPT-iOS/Projects/Modules/Networks/Sources/Foundation/AuthInterceptor.swift
@@ -0,0 +1,51 @@
+//
+//  AuthInterceptor.swift
+//  Networks
+//
+//  Created by 장석우 on 6/21/25.
+//  Copyright © 2025 SOPT-iOS. All rights reserved.
+//
+
+import Foundation
+
+import Core
+
+import Alamofire
+
+
+public final class AuthInterceptor: RequestInterceptor {
+    
+    public typealias AdapterResult = Swift.Result<URLRequest, Error>
+    
+    public typealias AccessTokenClosure = (@Sendable () -> String)
+    
+    public let accessTokenClosure: AccessTokenClosure
+    
+    public init(
+        accessTokenClosure: @escaping AccessTokenClosure = { UserDefaultKeyList.Auth.appAccessToken ?? ""}
+    ) {
+        self.accessTokenClosure = accessTokenClosure
+    }
+    
+    public func adapt(_ urlRequest: URLRequest, for session: Alamofire.Session, completion: @escaping (Result<URLRequest, Error>) -> Void) {
+        var adaptedRequest = urlRequest
+        validateHeader(&adaptedRequest)
+        completion(.success(adaptedRequest))
+    }
+    
+    // Note: 토큰 재발급 시 AccessToken 갱신
+    // @준호
+    private func validateHeader(_ urlRequest: inout URLRequest) {
+        let headers = urlRequest.headers.map {
+            guard $0.name == "Authorization" else {
+                return $0
+            }
+            return HTTPHeader(name: $0.name, value: accessTokenClosure())
+        }
+        urlRequest.headers = HTTPHeaders(headers)
+    }
+    
+    public func retry(_ request: Alamofire.Request, for session: Alamofire.Session, dueTo error: Swift.Error, completion: @escaping (RetryResult) -> Void) {
+        completion(.doNotRetryWithError(error))
+    }
+}
