f8278cdf2d790f61cc97ffe695ba898ea66cee58
Melt <seokwoo2000@naver.com>
2025-06-21 21:07:10 +0900

    [Feat] #645 - Interceptor객체의 accessToken및 reissue 동작에 대한 다형성 보장

---
+++ SOPT-iOS/Projects/Modules/Networks/Sources/Foundation/DefaultInterceptor.swift
@@ -0,0 +1,71 @@
+//
+//  AlamoInterceptor.swift
+//  Network
+//
+//  Created by Junho Lee on 2023/04/17.
+//  Copyright © 2023 SOPT-iOS. All rights reserved.
+//
+
+import Foundation
+
+import Core
+
+import Alamofire
+
+public final class DefaultInterceptor: RequestInterceptor {
+    
+    public typealias AdapterResult = Swift.Result<URLRequest, Error>
+    
+    public typealias ReissueClosure = (@Sendable (@escaping (Bool) -> Void) -> Void)
+    
+    public typealias AccessTokenClosure = (@Sendable () -> String)
+    
+    public let reissuance: ReissueClosure
+    
+    public let accessTokenClosure: AccessTokenClosure
+    
+    public init(
+        reissuance: @escaping ReissueClosure,
+        accessTokenClosure: @escaping AccessTokenClosure
+    ) {
+        self.reissuance = reissuance
+        self.accessTokenClosure = accessTokenClosure
+    }
+    
+    public func adapt(_ urlRequest: URLRequest, for session: Alamofire.Session, completion: @escaping (Result<URLRequest, Error>) -> Void) {
+        var adaptedRequest = urlRequest
+        validateHeader(&adaptedRequest)
+        completion(.success(adaptedRequest))
+    }
+    
+    // Note: 토큰 재발급 시 AccessToken 갱신
+    // @준호
+    private func validateHeader(_ urlRequest: inout URLRequest) {
+        let headers = urlRequest.headers.map {
+            guard $0.name == "Authorization" else {
+                return $0
+            }
+            return HTTPHeader(name: $0.name, value: accessTokenClosure())
+        }
+        urlRequest.headers = HTTPHeaders(headers)
+    }
+    
+    public func retry(_ request: Alamofire.Request, for session: Alamofire.Session, dueTo error: Swift.Error, completion: @escaping (RetryResult) -> Void) {
+        // token 재발급 API가 아니며 && 로그인 실패가 아니며 && 토큰이 만료된 경우(401)
+        guard let response = request.task?.response as? HTTPURLResponse,
+              response.statusCode == 401 else {
+            completion(.doNotRetryWithError(error))
+            return
+        }
+        
+        reissuance() { reissuanceSuccessed in
+            if reissuanceSuccessed {
+                print("토큰 갱신 성공: ", request.request?.url)
+                completion(.retry)
+            } else {
+                print("토큰 갱신 실패: ", request.request?.url)
+                completion(.doNotRetryWithError(APIError.tokenReissuanceFailed))
+            }
+        }
+    }
+}
