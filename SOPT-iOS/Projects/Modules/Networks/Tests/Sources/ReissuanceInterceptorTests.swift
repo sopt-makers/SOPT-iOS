beaffbf196bfbb4775ef2d65ce733ad1b325cb12
Melt <seokwoo2000@naver.com>
2025-06-24 21:11:17 +0900

    [Test] #646 - statusCode가_401일때_reissuance에_실패하면_기존API를_retry하지않는다

--- SOPT-iOS/Projects/Modules/Networks/Tests/Sources/ReissuanceInterceptorTests.swift
+++ SOPT-iOS/Projects/Modules/Networks/Tests/Sources/ReissuanceInterceptorTests.swift
@@ -28,7 +28,7 @@ struct ReissuanceInterceptorTests {
                 accessTokenClosure: { "" },
                 reissuance: reissunance
             )
-
+            
             let session = Session(interceptor: sut)
             let request = Alamofire.Request(
                 underlyingQueue: .main,
@@ -51,7 +51,51 @@ struct ReissuanceInterceptorTests {
                     Issue.record()
                 }
             }
+            
+            try? await _Concurrency.Task.sleep(for: .seconds(5))
         }
     }
+    
+    @Test
+    func statusCode가_401일때_reissuance에_실패하면_기존API를_retry하지않는다() async throws {
+        await confirmation { confirmation in
+            
+            // Given
+            let afError = AFError.responseValidationFailed(reason: .unacceptableStatusCode(code: 401))
+            let reissunance: ReissuanceInterceptor.ReissueClosure = { $0(false) }
+            
+            let sut = ReissuanceInterceptor(
+                accessTokenClosure: { "" },
+                reissuance: reissunance
+            )
+            
+            let session = Session(interceptor: sut)
+            
+            let request = Alamofire.Request(
+                underlyingQueue: .main,
+                serializationQueue: .main,
+                eventMonitor: nil,
+                interceptor: sut,
+                delegate: session
+            )
+            
+            // When
+            session.retryResult(
+                for: request,
+                dueTo: afError
+            ) { retryResult in
+                // Then
+                switch retryResult {
+                case .retry, .retryWithDelay:
+                    Issue.record()
+                case .doNotRetry, .doNotRetryWithError:
+                    confirmation()
+                }
+            }
+            try? await _Concurrency.Task.sleep(for: .seconds(5))
+        }
+    }
+    
+    
 }
 
