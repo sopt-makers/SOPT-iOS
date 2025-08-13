05ead926f0a6747b8114b27346b09aa61e276832
dlwogus0128 <79050615+dlwogus0128@users.noreply.github.com>
2025-08-13 17:54:40 +0900

    [Fix] #676 - 코드 리뷰 반영
    이미지가 svg일 경우 early return

--- SOPT-iOS/Projects/Core/Sources/Utils/setImage.swift
+++ SOPT-iOS/Projects/Core/Sources/Utils/setImage.swift
@@ -15,12 +15,22 @@ public extension UIImageView {
             .addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed) 
         else {
             print("URL 인코딩 실패")
+            self.image = placeholder
+            completion?(nil)
             return
         }
-
-        let cache = ImageCache.default
-        if urlString == "" {
+        
+        // SVG 이미지일 경우 검사
+        if urlString.lowercased().hasSuffix(".svg") {
             self.image = placeholder
+            completion?(nil)
+            return
+        }
+        
+        let cache = ImageCache.default
+        if urlString.isEmpty {
+            self.image = placeholder
+            completion?(nil)
         } else {
             cache.retrieveImage(forKey: urlString) { result in
                 result.success { imageCache in
