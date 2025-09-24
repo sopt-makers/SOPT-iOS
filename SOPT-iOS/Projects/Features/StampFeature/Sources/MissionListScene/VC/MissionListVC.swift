b9aea558124e4b6f6d9c92e327c298dd49304b25
hyeonju <shj040201@naver.com>
2025-09-24 19:04:21 +0900

    [chore]#704 - 개인별 랭킹으로 변경됨에 따른 함수 파라미터 및 네이밍 변경

--- SOPT-iOS/Projects/Features/StampFeature/Sources/MissionListScene/VC/MissionListVC.swift
+++ SOPT-iOS/Projects/Features/StampFeature/Sources/MissionListScene/VC/MissionListVC.swift
@@ -312,7 +312,7 @@ extension MissionListVC {
                 
                 self?.usersActiveGenerationStatus = generationStatus
                 self?.remakeButtonConstraint()
-                self?.configureCurrentGenerationButton(with: String(describing: generationStatus.currentGeneration))
+                self?.configurePersonalRankingButton()
             }.store(in: self.cancelBag)
         
         output.needNetworkAlert
@@ -421,7 +421,7 @@ extension MissionListVC {
         self.floatingButtonStackView.addArrangedSubviews(self.currentGenerationRankFloatingButton, self.partRankingFloatingButton)
     }
     
-    private func configureCurrentGenerationButton(with generation: String) {
+    private func configurePersonalRankingButton() {
         let attributedStr = NSMutableAttributedString(string: "개인별 랭킹")
         attributedStr.addAttribute(NSAttributedString.Key.kern, value: 0, range: NSMakeRange(0, attributedStr.length))
         attributedStr.addAttribute(NSAttributedString.Key.foregroundColor, value: DSKitColors.Color.black, range: NSMakeRange(0, attributedStr.length))
