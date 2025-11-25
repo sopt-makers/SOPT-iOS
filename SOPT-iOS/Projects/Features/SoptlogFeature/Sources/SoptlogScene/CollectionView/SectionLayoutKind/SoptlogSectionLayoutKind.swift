0de088e7de96daf3ed24527c2f9f0f6a9c63f1ff
yungu0010 <dbstj16@sookmyung.ac.kr>
2025-11-26 04:09:18 +0900

    [Feat] #759 - 섹션 정보를 담은 enum 추가

---
+++ SOPT-iOS/Projects/Features/SoptlogFeature/Sources/SoptlogScene/CollectionView/SectionLayoutKind/SoptlogSectionLayoutKind.swift
@@ -0,0 +1,35 @@
+//
+//  SoptlogSectionLayoutKind.swift
+//  SoptlogFeature
+//
+//  Created by 강윤서 on 11/25/24.
+//  Copyright © 2024 SOPT-iOS. All rights reserved.
+//
+
+import Foundation
+
+import Core
+
+enum SoptlogSectionLayoutKind: Int, CaseIterable {
+    case logo = 0
+    case soptampLog
+    case pokeLog
+    case banner
+    
+    var title: String {
+        switch self {
+        case .soptampLog: return I18N.Soptlog.soptamp
+        case .pokeLog: return I18N.Soptlog.poke
+        default: return ""
+        }
+    }
+    
+    var numberOfItems: Int {
+        switch self {
+        case .logo: return 1
+        case .soptampLog: return 4
+        case .pokeLog: return 4
+        case .banner: return 1
+        }
+    }
+}
