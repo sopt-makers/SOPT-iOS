8ec6fcac24ee51d5df6046b64bcc4b6e7f2fb8fc
yungu0010 <dbstj16@sookmyung.ac.kr>
2025-11-26 03:56:32 +0900

    [Feat] #759 - 마이 솝트로그 이미지 셀 구현

---
+++ SOPT-iOS/Projects/Features/SoptlogFeature/Sources/SoptlogScene/Cell/SoptlogImageCVC.swift
@@ -0,0 +1,57 @@
+//
+//  SoptlogImageCVC.swift
+//  SoptlogFeature
+//
+//  Created by 강윤서 on 11/25/24.
+//  Copyright © 2024 SOPT-iOS. All rights reserved.
+//
+
+import UIKit
+
+import Core
+import DSKit
+
+final class SoptlogImageCVC: UICollectionViewCell {
+    
+    // MARK: - UI Components
+    
+    private let imageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFit
+        return imageView
+    }()
+    
+    // MARK: - Initialization
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setUI()
+        setLayout()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - UI & Layout
+    
+    private func setUI() {
+        contentView.backgroundColor = .clear
+    }
+    
+    private func setLayout() {
+        contentView.addSubview(imageView)
+        
+        imageView.snp.makeConstraints { make in
+            make.top.equalToSuperview().inset(20)
+            make.horizontalEdges.equalToSuperview().inset(20)
+            make.bottom.equalToSuperview().inset(8)
+        }
+    }
+    
+    // MARK: - Configuration
+    
+    func configure(image: UIImage?) {
+        imageView.image = image
+    }
+}
