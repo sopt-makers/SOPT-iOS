5a62f3d63dc14f92f49e2ff990479b23fea84d60
Yunseo Kang <65678579+yungu0010@users.noreply.github.com>
2025-12-09 00:59:55 +0900

    [Del] #775 - WebFeature 의존성 제거

--- SOPT-iOS/Projects/Features/SoptlogFeature/Project.swift
+++ SOPT-iOS/Projects/Features/SoptlogFeature/Project.swift
@@ -13,7 +13,6 @@ let project = Project.makeModule(
     name: "SoptlogFeature",
     targets: [.unitTest, .staticFramework, .demo, .interface],
     interfaceDependencies: [
-        .Features.Web.Feature,
         .Features.Poke.Interface
     ]
 )
