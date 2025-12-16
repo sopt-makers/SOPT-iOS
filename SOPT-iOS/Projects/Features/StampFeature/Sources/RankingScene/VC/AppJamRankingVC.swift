9874b21f85ecd6fe0280c17842be1283b3f2f1ee
yungu0010 <dbstj16@sookmyung.ac.kr>
2025-12-16 19:59:53 +0900

    [Chore] #794 - 랭킹 뷰 폴더링

---
+++ SOPT-iOS/Projects/Features/StampFeature/Sources/RankingScene/VC/AppJamRankingVC.swift
@@ -0,0 +1,17 @@
+//
+//  AppJamRankingVC.swift
+//  StampFeature
+//
+//  Created by 강윤서 on 12/16/25.
+//  Copyright © 2025 SOPT-iOS. All rights reserved.
+//
+
+import UIKit
+
+final class AppJamRankingVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
