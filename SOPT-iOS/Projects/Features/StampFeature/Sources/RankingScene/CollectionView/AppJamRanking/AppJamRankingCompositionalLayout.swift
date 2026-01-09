3e995e05a90f97fb97a0426e953a5a9946650a0c
yungu0010 <dbstj16@sookmyung.ac.kr>
2026-01-09 23:26:59 +0900

    [Del] #789 - 불필요한 레이아웃 코드 제거
    - .estimated의 경우 NSDirectionalEdgeInsets이 무시됨
    - 또한 이미 HeaderView 내의 레이아웃에서 top inset을 설정하고 있음

--- SOPT-iOS/Projects/Features/StampFeature/Sources/RankingScene/CollectionView/AppJamRanking/AppJamRankingCompositionalLayout.swift
+++ SOPT-iOS/Projects/Features/StampFeature/Sources/RankingScene/CollectionView/AppJamRanking/AppJamRankingCompositionalLayout.swift
@@ -49,7 +49,6 @@ extension AppJamRankingVC {
             elementKind: UICollectionView.elementKindSectionHeader,
             alignment: .top
         )
-        header.contentInsets = NSDirectionalEdgeInsets(top: 16, leading: 0, bottom: 0, trailing: 0)
         
         section.boundarySupplementaryItems = [header]
         
