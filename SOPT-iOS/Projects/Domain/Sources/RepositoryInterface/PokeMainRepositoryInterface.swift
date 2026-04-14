267a1abb4b61feb14e72198e6c47d851b5a10c5f
kwonseokki <kwons4163@naver.com>
2026-04-14 18:47:36 +0900

    [Feat] #845 -콕찌르기 온보딩 여부 판별을 위한 getIsNewUser 메서드 추가

--- SOPT-iOS/Projects/Domain/Sources/RepositoryInterface/PokeMainRepositoryInterface.swift
+++ SOPT-iOS/Projects/Domain/Sources/RepositoryInterface/PokeMainRepositoryInterface.swift
@@ -12,4 +12,5 @@ public protocol PokeMainRepositoryInterface: PokeRepositoryInterface {
     func getWhoPokeToMe() -> AnyPublisher<PokeUserModel?, Error>
     func getFriend() -> AnyPublisher<[PokeUserModel], Error>
     func getFriendRandomUser(randomType: String, size: Int) -> AnyPublisher<PokeFriendRandomUserModel, Error>
+    func getIsNewUser() -> AnyPublisher<Bool, Error>
 }
