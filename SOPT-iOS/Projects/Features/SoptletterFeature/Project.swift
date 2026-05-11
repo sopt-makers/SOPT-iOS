8e314c1409806c2f2a1ccdfa8c293d2a97f826e6
yungu0010 <dbstj16@sookmyung.ac.kr>
2026-05-11 18:44:31 +0900

    [Chore] #875 - 솝레터 모듈 생성

---
+++ SOPT-iOS/Projects/Features/SoptletterFeature/Project.swift
@@ -0,0 +1,19 @@
+//
+//  Project.swift
+//  ProjectDescriptionHelpers
+//
+//  Created by yungu0010 on 05/11/26.
+//
+
+import ProjectDescription
+import ProjectDescriptionHelpers
+import DependencyPlugin
+
+let project = Project.makeModule(
+    name: "SoptletterFeature",
+    targets: [.unitTest, .staticFramework, .demo, .interface],
+    internalDependencies: [
+    ],
+    interfaceDependencies: [  
+    ]
+)
