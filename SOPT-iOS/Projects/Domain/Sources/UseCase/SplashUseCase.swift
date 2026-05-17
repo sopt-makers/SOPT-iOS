2b27439604441320dd41a2c6714b97d806fb2f27
JURI <80569323+juri123123@users.noreply.github.com>
2026-05-17 15:53:05 +0900

    [Fix] #868 - 코드리뷰 반영

--- SOPT-iOS/Projects/Domain/Sources/UseCase/SplashUseCase.swift
+++ SOPT-iOS/Projects/Domain/Sources/UseCase/SplashUseCase.swift
@@ -97,9 +97,8 @@ extension DefaultSplashUseCase: SplashUseCase {
         let needForceUpdate = currentAppVersion.compare(minimumVersion, options: .numeric) == .orderedAscending
         let needOptionalUpdate = currentAppVersion.compare(appStoreVersion, options: .numeric) == .orderedAscending
         
-        return .none
-//        return needForceUpdate ? .forcedUpdate(forcedUpdateData.appNotice) :
-//                needOptionalUpdate ? .optionalUpdate(optionalUpdateData) : .none
+        return needForceUpdate ? .forcedUpdate(forcedUpdateData.appNotice) :
+                needOptionalUpdate ? .optionalUpdate(optionalUpdateData) : .none
     }
     
     private func handleUpdateType(_ type: UpdateType) throws {
