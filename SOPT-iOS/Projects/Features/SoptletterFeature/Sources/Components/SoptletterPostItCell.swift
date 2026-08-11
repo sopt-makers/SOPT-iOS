4f6f8666c00bb06b040dacd03a4c831943f5cc76
yungu0010 <dbstj169@gmail.com>
2026-08-11 17:01:44 +0900

    [Refactor] #951 - SoptletterPostItCell의 중복 UIColor(hex:) 제거
    Core/Sources/Extension/UIKit+/UIColor+.swift에 이미 동일한
    public convenience init(hex:)가 있어서 중복 선언 제거

--- SOPT-iOS/Projects/Features/SoptletterFeature/Sources/Components/SoptletterPostItCell.swift
+++ SOPT-iOS/Projects/Features/SoptletterFeature/Sources/Components/SoptletterPostItCell.swift
@@ -114,22 +114,6 @@ extension SoptletterPostItCell {
     }
 }
 
-extension UIColor {
-    convenience init(hex: String) {
-        var hexString = hex.trimmingCharacters(in: .whitespacesAndNewlines)
-        hexString = hexString.replacingOccurrences(of: "#", with: "")
-
-        var rgb: UInt64 = 0
-        Scanner(string: hexString).scanHexInt64(&rgb)
-
-        let r = CGFloat((rgb & 0xFF0000) >> 16) / 255.0
-        let g = CGFloat((rgb & 0x00FF00) >> 8) / 255.0
-        let b = CGFloat(rgb & 0x0000FF) / 255.0
-
-        self.init(red: r, green: g, blue: b, alpha: 1.0)
-    }
-}
-
 enum SoptletterShapeMapping: String {
     case sharp = "SHARP"
     case cloud = "CLOUD"
