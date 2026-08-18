5d818bc9afccf1ef8d6967b66d3887ed6ea683d6
yungu0010 <dbstj169@gmail.com>
2026-08-18 13:31:50 +0900

    [Chore] #956 - MDS 의존성을 1.1.1 버전으로 업데이트

--- SOPT-iOS/Tuist/Package.swift
+++ SOPT-iOS/Tuist/Package.swift
@@ -45,6 +45,6 @@ let package = Package(
         .package(url: "https://github.com/amplitude/Amplitude-Swift", from: "1.11.10"),
         .package(url: "https://github.com/firebase/firebase-ios-sdk.git", from: "11.12.0"),
         .package(url: "https://github.com/google/GoogleSignIn-iOS.git", from: "9.0.0"),
-        .package(url: "https://github.com/sopt-makers/SOPT-iOS-MDS", from: "1.1.0"),
+        .package(url: "https://github.com/sopt-makers/SOPT-iOS-MDS.git", from: "1.1.1"),
     ]
 )
