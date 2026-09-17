9780d255f081a0ed930afe003ee99affab492ef6
yeonee <leenayeon@sookmyung.ac.kr>
2026-09-17 23:58:10 +0900

    [chore] - #986 true로 변경

--- SOPT-iOS/Projects/Features/WebFeature/Sources/SOPTWebView.swift
+++ SOPT-iOS/Projects/Features/WebFeature/Sources/SOPTWebView.swift
@@ -71,7 +71,7 @@ public final class SOPTWebView: UIViewController, SOPTWebViewControllable {
         self.webView = WKWebView(frame: .zero, configuration: configuration).then {
             $0.allowsBackForwardNavigationGestures = config.allowsBackForwardNavigationGestures
             $0.customUserAgent = "SOPT-iOS"
-            $0.isOpaque = false
+            $0.isOpaque = true
             $0.backgroundColor = DSKitAsset.Colors.black100.color
             $0.scrollView.backgroundColor = DSKitAsset.Colors.black100.color
             
