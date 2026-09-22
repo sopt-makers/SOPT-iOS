5bff5900c96f332c5df725b56b9d48ed0604ed93
yeonee <leenayeon@sookmyung.ac.kr>
2026-09-22 16:28:05 +0900

    [Fix] - #993 corner radius full 주고 있는 것 수정

--- SOPT-iOS/Projects/Features/AuthFeature/Sources/ChangeSocialAccountScene/ChangeSocialAccountVC.swift
+++ SOPT-iOS/Projects/Features/AuthFeature/Sources/ChangeSocialAccountScene/ChangeSocialAccountVC.swift
@@ -70,7 +70,7 @@ public class ChangeSocialAccountVC: UIViewController, ChangeSocialAccountViewCon
         $0.tintColor = SemanticColor.Fg.Neutral.bold
         $0.contentMode = .scaleAspectFit
         $0.backgroundColor = SemanticColor.Bg.Secondary.default
-        $0.layer.cornerRadius = BaseRadius.Base.full
+        $0.layer.cornerRadius = 11 // firstStepIndicator.circleView(22x22)와 동일한 높이의 절반
         $0.layer.masksToBounds = true
         $0.isHidden = true
     }
